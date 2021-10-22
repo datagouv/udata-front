5ee635f218b028a35251d754a8ed9b64135acbc9
maudetes <maudet.estelle@gmail.com>
2021-10-22 10:38:23 +0200

    Keep only GET related code

--- udata_front/views/oauth.py
+++ udata_front/views/oauth.py
@@ -16,20 +16,11 @@ oauth = AuthorizationServer()
 @login_required
 def authorize(*args, **kwargs):
     '''Override /authorize GET route to render udata-front template'''
-    if request.method == 'GET':
-        try:
-            grant = oauth.validate_consent_request(end_user=current_user)
-        except OAuth2Error as error:
-            return error.error
-        # Bypass authorization screen for internal clients
-        if grant.client.internal:
-            return oauth.create_authorization_response(grant_user=current_user)
-        return theme.render('api/oauth_authorize.html', grant=grant)
-    elif request.method == 'POST':
-        accept = 'accept' in request.form
-        decline = 'decline' in request.form
-        if accept and not decline:
-            grant_user = current_user
-        else:
-            grant_user = None
-        return oauth.create_authorization_response(grant_user=grant_user)
+    try:
+        grant = oauth.validate_consent_request(end_user=current_user)
+    except OAuth2Error as error:
+        return error.error
+    # Bypass authorization screen for internal clients
+    if grant.client.internal:
+        return oauth.create_authorization_response(grant_user=current_user)
+    return theme.render('api/oauth_authorize.html', grant=grant)
