40f9616296907ef8591f5f39160eb506ac4ba82e
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-02-02 13:35:03 +0100

    refac: remove unfilled props

--- theme/js/components/dataset/card.vue
+++ theme/js/components/dataset/card.vue
@@ -2,10 +2,6 @@
   <article class="dataset-card compact">
     <div class="card-logo">
       <Placeholder type="reuse" :src="image_url" :alt="title" />
-      <div class="logo-badge">
-        <span v-html="lock" v-if="private" />
-        <span v-html="certified" v-else-if="organizationCertified" />
-      </div>
     </div>
     <div class="card-data">
       <h3 class="card-body h4">
@@ -20,9 +16,6 @@
 
 <script>
 import Placeholder from "../utils/placeholder";
-import certified from "svg/certified.svg";
-import lock from "svg/private.svg";
-import useOrganizationCertified from "../../composables/useOrganizationCertified";
 
 export default {
   components: {
@@ -31,16 +24,6 @@ export default {
   props: {
     title: String,
     image_url: String,
-    organization: Object,
-    private: Boolean,
   },
-  setup(props) {
-    const {organizationCertified} = useOrganizationCertified(props.organization)
-    return {
-      certified,
-      lock,
-      organizationCertified,
-    };
-  }
 };
 </script>
