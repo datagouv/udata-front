20ed9389cde1e9fce2b13deb506d0d65267d5f7a
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-02-24 11:22:57 +0100

    feat: show identicon on search results

--- theme/js/components/discussions/avatar.vue
+++ theme/js/components/discussions/avatar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="avatar">
-    <img :src="avatarUrl" alt="" />
+    <img :src="avatarUrl" loading="lazy" alt="" />
   </div>
 </template>
 
@@ -10,9 +10,13 @@ import useUserAvatar from "../../composables/useUserAvatar";
 export default {
   props: {
     user: Object,
+    size: {
+      type: Number,
+      default: 40,
+    },
   },
   setup(props) {
-    const avatarUrl = useUserAvatar(props.user);
+    const avatarUrl = useUserAvatar(props.user, props.size);
     return {
       avatarUrl
     }
--- theme/js/composables/useUserAvatar.js
+++ theme/js/composables/useUserAvatar.js
@@ -1,6 +1,6 @@
 import config from "../config";
 
-export default function useUserAvatar(user) {
-  const getIdenticon = () => `${config.api_root}avatars/${id}/${size}`;
-  return user.avatar || getIdenticon(user.id);
+export default function useUserAvatar(user, size) {
+  const getIdenticon = (id, size) => `${config.api_root}avatars/${id}/${size}`;
+  return user.avatar || getIdenticon(user.id, size);
 }
