8b7238b2b7df13fc813489f5d8529ffcae23fb4f
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-03-08 14:02:25 +0100

    feat: update resource format dict

--- theme/js/composables/useResourceImage.js
+++ theme/js/composables/useResourceImage.js
@@ -1,4 +1,4 @@
-import api from 'svg/resources/code.svg';
+import code from 'svg/resources/code.svg';
 import archive from 'svg/resources/archive.svg';
 import documentation from 'svg/resources/documentation.svg';
 import file from 'svg/resources/file.svg';
@@ -10,19 +10,58 @@ import table from 'svg/resources/table.svg';
  */
 export default function useResourceImage(resource) {
   switch (resource.format?.trim()?.toLowerCase()) {
+    case 'txt':
+    case 'pdf':
+    case 'rtf':
+    case 'odt':
     case 'doc':
-    case 'pdf': return documentation;
+    case 'docx':
+    case 'epub':
+      return documentation;
     case 'json':
     case 'sql':
-    case 'xml': return api;
-    case '7z':
-    case 'tar.gz':
+    case 'xml':
+    case 'xsd':
+    case 'shp':
+    case 'kml':
+    case 'kmz':
+    case 'gpx':
+    case 'shx':
+    case 'ovr':
+    case 'geojson':
+    case 'gpkg':
+    case 'grib2':
+    case 'dbf':
+    case 'prj':
+    case 'sqlite':
+    case 'db':
+    case 'sbn':
+    case 'sbx':
+    case 'cpg':
+    case 'lyr':
+    case 'owl':
+    case 'dxf':
+    case 'ics':
+    case 'rdf':
+    case 'ttl':
+    case 'n3':
+      return code;
+    case 'tar':
+    case 'gz':
+    case 'tgz':
     case 'rar':
-    case 'zip': return archive;
-    case 'url': return link;
+    case 'zip':
+     case '7z':
+     case 'xz':
+     case 'bz2':
+      return archive;
+    case 'url':
+      return link;
     case 'csv':
+    case 'ods':
     case 'xls':
-    case 'xlsx': return table;
+    case 'xlsx':
+      return table;
     default:
       return file;
   }
