cf4c79cf04801582f44c46728398933ac5687d36
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-05-24 15:04:02 +0200

    feat: update instruction on search pages

--- theme/js/components/search/search.vue
+++ theme/js/components/search/search.vue
@@ -4,7 +4,7 @@
       <SearchInput
         :onChange="handleSearchChange"
         :value="queryString"
-        :placeholder="$t('Search for data...')"
+        :placeholder="$t('Ex. 2022 presiential election')"
       />
     </div>
     <div class="fr-grid-row fr-mt-1w fr-mt-md-5v">
