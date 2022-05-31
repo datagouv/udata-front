1dcd9285993c936470920feb0d6910103b409f74
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-05-31 10:39:22 +0200

    refac: use global scope for i18n-t

--- theme/js/components/search/menu-search-option.vue
+++ theme/js/components/search/menu-search-option.vue
@@ -3,7 +3,7 @@
     <div class="fr-grid-row fr-grid-row--middle">
       <div aria-hidden="true" class="fr-col-auto fr-mr-2w" v-html="icon">
       </div>
-      <i18n-t keypath="Search X in Y" class="fr-col" tag="div" v-if="query">
+      <i18n-t keypath="Search X in Y" class="fr-col" tag="div" scope="global" v-if="query">
         <template #query>
           <em>{{query}}</em>
         </template>
@@ -11,7 +11,7 @@
           <strong>{{type}}</strong>
         </template>
       </i18n-t>
-      <i18n-t keypath="Start typing to search in X" class="fr-col" tag="div" v-else>
+      <i18n-t keypath="Start typing to search in X" class="fr-col" tag="div" scope="global" v-else>
         <template #type>
           <strong>{{type}}</strong>
         </template>
