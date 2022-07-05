f5f93fb61bb75a7268f564cb5915f2ddcb9eb74a
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-07-05 12:07:20 +0200

    feat: use real quality score

--- theme/js/components/dataset/search-result.vue
+++ theme/js/components/dataset/search-result.vue
@@ -47,7 +47,7 @@
           <p class="fr-mb-0 text-mention-grey">
             <span class="fr-icon-information-line" aria-hidden="true"></span>
             {{$t('Metadata quality:')}}
-            <QualityScore :score="8"/>
+            <QualityScore :score="quality.score"/>
             <template v-if="!externalSource">
               &mdash;
               {{ $t('Updated on {date}', {date: $filters.formatDate(last_modified)}) }}
@@ -129,6 +129,10 @@ export default defineComponent({
       required: true,
     },
     private: Boolean,
+    quality: {
+      type: Object,
+      required: true,
+    },
     resources: Object,
     title: {
       type: String,
