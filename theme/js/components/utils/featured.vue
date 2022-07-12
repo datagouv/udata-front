a8c4ee1e5acd89912db49392c273c90c5c1aeda5
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-07-12 10:47:25 +0200

    refactor(dataset): :recycle: move to composition API

--- theme/js/components/utils/featured.vue
+++ theme/js/components/utils/featured.vue
@@ -1,38 +1,55 @@
 <template>
 <div class="fr-toggle fr-toggle--label-left">
-    <input ref="toggleInput" @click="toggleFeatured" type="checkbox" class="fr-toggle__input" id="featured-toggle" :checked="_featured">
-    <label class="fr-toggle__label" for="featured-toggle" :data-fr-checked-label="$t('Unfeature this content')" :data-fr-unchecked-label="$t('Feature this content')"></label>
+    <input ref="toggleInput" @click="toggleFeatured" type="checkbox" class="fr-toggle__input" id="featured-toggle" :checked="isFeatured">
+    <label class="fr-toggle__label" for="featured-toggle" :data-fr-checked-label="t('Unfeature this content')" :data-fr-unchecked-label="t('Feature this content')"></label>
 </div>
 </template>
 
 <script>
-import {defineComponent} from "vue";
+import { defineComponent, onMounted, ref, Ref } from "vue";
+import { useI18n } from "vue-i18n";
+import { useToast } from "../../composables/useToast";
+import { api } from "../../plugins/api";
 export default defineComponent({
     name: "featured-button",
     props: {
-        subjectId: String,
-        subjectType: String,
-        featured: Boolean,
+        subjectId: {
+            type: String,
+            required: true,
+        },
+        subjectType: {
+            type: String,
+            required: true,
+        },
+        featured: {
+            type: Boolean,
+            required: true,
+        },
     },
-    data() {
-        return {
-            registration: null,
-            _featured: this.featured,
-        }
-    },
-    mounted() {
-        this.$refs.toggleInput.toggleAttribute('checked', this._featured);
-    },
-    methods: {
-        toggleFeatured() {
-            const method = this._featured ? 'delete' : 'post';
-            const url = `${this.subjectType.toLowerCase()}s/${this.subjectId}/featured/`;
-            this.$api[method](url)
-                .then(response => {
-                    this._featured = !this._featured;
-                })
-                .catch(() => this.$toast.error(this.$t('An error occurred while featuring this subject.')));
+    setup(props) {
+        const {t} = useI18n();
+        const toast = useToast();
+        const isFeatured = ref(props.featured);
+        /** @type {Ref<HTMLElement | null>} */
+        const toggleInput = ref(null);
 
+        const toggleFeatured = () => {
+            const method = isFeatured.value ? 'delete' : 'post';
+            const url = `${props.subjectType.toLowerCase()}s/${props.subjectId}/featured/`;
+            api[method](url)
+            .then(response => {
+                isFeatured.value = !isFeatured.value;
+            })
+            .catch(() => toast.error(t('An error occurred while featuring this subject.')));
+        }
+
+        onMounted(() => toggleInput.value?.toggleAttribute('checked', isFeatured.value));
+
+        return {
+            isFeatured,
+            t,
+            toggleFeatured,
+            toggleInput,
         }
     },
 });
