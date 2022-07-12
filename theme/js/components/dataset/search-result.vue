94b215f96b0c4db5fbb2f1ee7a0230971886e681
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-07-12 10:25:09 +0200

    style(dataset): :art: indent

--- theme/js/components/dataset/search-result.vue
+++ theme/js/components/dataset/search-result.vue
@@ -22,109 +22,109 @@
         </div>
       </div>
       <div class="fr-col">
-          <h4 class="fr-mb-1v">
-            <a :href="page" class="text-grey-500">
-              {{ title }}
-              <small v-if="acronym">{{ acronym }}</small>
-            </a>
-            <span
-              v-if="private"
-              class="badge grey-300 fr-ml-1w"
-            >
-              {{ $t('Private') }}
-            </span>
-          </h4>
-          <p class="fr-m-0 not-enlarged" v-if="organization || owner">
-            {{ $t('From') }} 
-            <a :href="page" v-if="organization">
-                <OrganizationNameWithCertificate :organization="organization" />
-            </a>
-            <template v-if="owner">{{ownerName}}</template>
-          </p>
-          <p class="fr-mt-1w fr-mb-2w fr-hidden fr-unhidden-sm">
-            {{ $filters.excerpt(description, 160) }}
-          </p>
-          <p class="fr-mb-0 text-mention-grey">
-            <Tooltip class="fr-hidden inline-sm">
-                <template #tooltip>
-                  <h5 class="fr-text--sm fr-my-0">{{$t("Metadata quality:")}}</h5>
-                  <QualityItem
-                    :passed="quality.dataset_description_quality"
-                    :messagePassed='$t("Data description filled")'
-                    :messageFailed='$t("Data description empty")'
-                    class="fr-my-1w"
-                  />
-                  <QualityItem
-                    :passed="quality.license"
-                    :messagePassed='$t("License filled")'
-                    :messageFailed='$t("No license set")'
-                    class="fr-my-1w"
-                  />
-                  <QualityItem
-                    :passed="quality.update_frequency && !!quality.update_fulfilled_in_time"
-                    :messagePassed='$t("Update frequency followed")'
-                    :messageFailed='quality.update_frequency ? $t("Update frequency not followed") : $t("Update frequency not set")'
-                    class="fr-my-1w"
-                  />
-                  <QualityItem
-                    :passed="quality.has_open_format"
-                    :messagePassed='$t("File formats are open")'
-                    :messageFailed='$t("File formats are closed")'
-                    class="fr-my-1w"
-                  />
-                  <QualityItem
-                    :passed="quality.temporal_coverage"
-                    :messagePassed='$t("Temporal coverage filled")'
-                    :messageFailed='$t("Temporal coverage not set")'
-                    class="fr-my-1w"
-                  />
-                  <QualityItem
-                    :passed="quality.spatial"
-                    :messagePassed='$t("Spatial coverage filled")'
-                    :messageFailed='$t("Spatial coverage not set")'
-                    class="fr-my-1w"
-                  />
-                  <div class="fr-grid-row fr-grid-row--right not-enlarged">
-                    <a
-                      href="https://guides.etalab.gouv.fr/qualite/documenter-les-donnees/"
-                      target="_blank"
-                    >
-                      {{$t("Learn more about this indicator")}}
-                    </a>
-                  </div>
-                </template>
-                <span class="fr-icon-info-line" aria-hidden="true"></span>
-                <span class="fr-reset-link fr-mx-1v">
-                  {{$t('Metadata quality:')}}
-                </span>
-                <QualityScore :score="quality.score"/>
-            </Tooltip>
-            <template v-if="!externalSource">
-              &mdash;
-              {{ $t('Updated on {date}', {date: $filters.formatDate(last_modified)}) }}
-            </template>
-          </p>
-        </div>
-      <ul class="fr-hidden fr-unhidden-sm fr-hidden-md fr-unhidden-lg fr-col-auto fr-tags-group fr-grid-row--bottom self-center flex-direction-column">
-            <li>
-              <span class="fr-tag">
-                <i18n-t keypath="{n} reuses" :plural="metrics.reuses || 0" scope="global">
-                  <template #n>
-                    <strong class="fr-mr-1v">{{metrics.reuses || 0}}</strong>
-                  </template>
-                </i18n-t>
-                </span>
-            </li>
-            <li>
-              <span class="fr-tag">
-                <i18n-t keypath="{n} favorites" :plural="metrics.followers || 0" scope="global">
-                  <template #n>
-                    <strong class="fr-mr-1v">{{metrics.followers}}</strong>
-                  </template>
-                </i18n-t>
+        <h4 class="fr-mb-1v">
+          <a :href="page" class="text-grey-500">
+            {{ title }}
+            <small v-if="acronym">{{ acronym }}</small>
+          </a>
+          <span
+            v-if="private"
+            class="badge grey-300 fr-ml-1w"
+          >
+            {{ $t('Private') }}
+          </span>
+        </h4>
+        <p class="fr-m-0 not-enlarged" v-if="organization || owner">
+          {{ $t('From') }} 
+          <a :href="page" v-if="organization">
+              <OrganizationNameWithCertificate :organization="organization" />
+          </a>
+          <template v-if="owner">{{ownerName}}</template>
+        </p>
+        <p class="fr-mt-1w fr-mb-2w fr-hidden fr-unhidden-sm">
+          {{ $filters.excerpt(description, 160) }}
+        </p>
+        <p class="fr-mb-0 text-mention-grey">
+          <Tooltip class="fr-hidden inline-sm">
+              <template #tooltip>
+                <h5 class="fr-text--sm fr-my-0">{{$t("Metadata quality:")}}</h5>
+                <QualityItem
+                  :passed="quality.dataset_description_quality"
+                  :messagePassed='$t("Data description filled")'
+                  :messageFailed='$t("Data description empty")'
+                  class="fr-my-1w"
+                />
+                <QualityItem
+                  :passed="quality.license"
+                  :messagePassed='$t("License filled")'
+                  :messageFailed='$t("No license set")'
+                  class="fr-my-1w"
+                />
+                <QualityItem
+                  :passed="quality.update_frequency && !!quality.update_fulfilled_in_time"
+                  :messagePassed='$t("Update frequency followed")'
+                  :messageFailed='quality.update_frequency ? $t("Update frequency not followed") : $t("Update frequency not set")'
+                  class="fr-my-1w"
+                />
+                <QualityItem
+                  :passed="quality.has_open_format"
+                  :messagePassed='$t("File formats are open")'
+                  :messageFailed='$t("File formats are closed")'
+                  class="fr-my-1w"
+                />
+                <QualityItem
+                  :passed="quality.temporal_coverage"
+                  :messagePassed='$t("Temporal coverage filled")'
+                  :messageFailed='$t("Temporal coverage not set")'
+                  class="fr-my-1w"
+                />
+                <QualityItem
+                  :passed="quality.spatial"
+                  :messagePassed='$t("Spatial coverage filled")'
+                  :messageFailed='$t("Spatial coverage not set")'
+                  class="fr-my-1w"
+                />
+                <div class="fr-grid-row fr-grid-row--right not-enlarged">
+                  <a
+                    href="https://guides.etalab.gouv.fr/qualite/documenter-les-donnees/"
+                    target="_blank"
+                  >
+                    {{$t("Learn more about this indicator")}}
+                  </a>
+                </div>
+              </template>
+              <span class="fr-icon-info-line" aria-hidden="true"></span>
+              <span class="fr-reset-link fr-mx-1v">
+                {{$t('Metadata quality:')}}
               </span>
-            </li>
-        </ul>
+              <QualityScore :score="quality.score"/>
+          </Tooltip>
+          <template v-if="!externalSource">
+            &mdash;
+            {{ $t('Updated on {date}', {date: $filters.formatDate(last_modified)}) }}
+          </template>
+        </p>
+      </div>
+      <ul class="fr-hidden fr-unhidden-sm fr-hidden-md fr-unhidden-lg fr-col-auto fr-tags-group fr-grid-row--bottom self-center flex-direction-column">
+        <li>
+          <span class="fr-tag">
+            <i18n-t keypath="{n} reuses" :plural="metrics.reuses || 0" scope="global">
+              <template #n>
+                <strong class="fr-mr-1v">{{metrics.reuses || 0}}</strong>
+              </template>
+            </i18n-t>
+            </span>
+        </li>
+        <li>
+          <span class="fr-tag">
+            <i18n-t keypath="{n} favorites" :plural="metrics.followers || 0" scope="global">
+              <template #n>
+                <strong class="fr-mr-1v">{{metrics.followers}}</strong>
+              </template>
+            </i18n-t>
+          </span>
+        </li>
+      </ul>
     </div>
   </article>
 </template>
