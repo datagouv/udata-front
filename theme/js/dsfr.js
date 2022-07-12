42c2c6d2b12084095b5854ef93b610481bbee2b1
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-07-12 10:54:09 +0200

    docs(dsfr): :memo: add documentation about DSFR mode

--- theme/js/dsfr.js
+++ theme/js/dsfr.js
@@ -1,3 +1,8 @@
+/**
+ * When loading, DSFR check for custom settings in `window.dsfr`
+ * Then, they overwrite it with the DSFR object
+ * We use a manual mode to load all our vue instances before starting the DSFR engine.
+ */
 window.dsfr = {
     mode: 'manual',
 }
