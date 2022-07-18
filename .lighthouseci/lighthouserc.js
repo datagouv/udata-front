8351ca7eb8583846135b4eb8515aaea75eb85718
maudetes <maudet.estelle@gmail.com>
2022-07-18 19:31:58 +0200

    Add needed middlewares and udata to test lighthouse

---
+++ .lighthouseci/lighthouserc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ci: {
+      collect: {
+        startServerCommand: 'udata serve',
+        url: ['http://localhost:7000']
+      },
+      upload: {
+        /* Add configuration here */
+      },
+    },
+  };
