d525732de5f38d5f92bb8f325a50823ae8130768
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-08-30 15:03:10 +0200

    feat: :sparkles: handle captchEtat bearer token

--- udata_front/api.py
+++ udata_front/api.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timedelta
 from flask import current_app
 
+import logging
 import requests
 
 from udata.app import cache
-from udata.api import apiv2, API, log
+from udata.api import apiv2, API
+
+log = logging.getLogger(__name__)
 
 ns = apiv2.namespace('captchetat', 'CaptchEtat related operations')
 captchetat_parser = apiv2.parser()
@@ -16,19 +19,16 @@ captchetat_parser.add_argument('cs', type=str, location='args')
 @ns.route('/', endpoint='captchetat')
 class CaptchEtatAPI(API):
     
-    token_expired_at = None
-    cache_key = 'captchetat-bearer-token'
+    token_cache_key = 'captchetat-bearer-token'
 
-    def check_bearer_expired(self, forced):
-        print(self)
-        print(datetime.now())
-        return self.token_expired_at > datetime.now() if not forced and self.token_expired_at else True
-
-    @cache.cached(key_prefix=cache_key, unless=check_bearer_expired)
     def bearer_token(self):
+        '''Get CaptchEtat bearer token from cache or get a new one from captchEtat Oauth server'''
         url = current_app.config.get('CAPTCHETAT_OAUTH_BASE_URL') + '/api/oauth/token'
-        previous_value = cache.get(self.cache_key) or ""
-        print(url)
+        previous_value = cache.get(self.token_cache_key)
+        access_token = ""
+        if(previous_value):
+            return previous_value
+        log.info(f'New access token requested from {url}')
         try: 
             oauth = requests.post(url, data={
                 'grant_type': 'client_credentials',
@@ -37,11 +37,10 @@ class CaptchEtatAPI(API):
                 'client_secret': current_app.config.get('CAPTCHETAT_CLIENT_SECRET')
             })
             body = oauth.json()
-            access_token = body.get('access_token', previous_value)
-            self.token_expired_at = datetime.now() + timedelta(seconds=body.get('expires_in', 0))
+            access_token = body.get('access_token', "")
+            cache.set(self.token_cache_key, access_token, timeout=body.get('expires_in', 0))
         except requests.exceptions.RequestException as e:
             log.exception(f'Error while getting access token from {url}')
-            access_token = previous_value
         else:
             return access_token
 
