afd55d9b7622d12412bf4eb0128216bdf24a709e
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-08-30 14:06:55 +0200

    feat: :construction: bearer with unless

--- udata_front/api.py
+++ udata_front/api.py
@@ -1,8 +1,10 @@
+from datetime import datetime, timedelta
 from flask import current_app
 
 import requests
 
-from udata.api import apiv2, API
+from udata.app import cache
+from udata.api import apiv2, API, log
 
 ns = apiv2.namespace('captchetat', 'CaptchEtat related operations')
 captchetat_parser = apiv2.parser()
@@ -13,22 +15,42 @@ captchetat_parser.add_argument('t', type=str, location='args')
 captchetat_parser.add_argument('cs', type=str, location='args')
 @ns.route('/', endpoint='captchetat')
 class CaptchEtatAPI(API):
-    @property
+    
+    token_expired_at = None
+    cache_key = 'captchetat-bearer-token'
+
+    def check_bearer_expired(self, forced):
+        print(self)
+        print(datetime.now())
+        return self.token_expired_at > datetime.now() if not forced and self.token_expired_at else True
+
+    @cache.cached(key_prefix=cache_key, unless=check_bearer_expired)
     def bearer_token(self):
-        oauth = requests.post(current_app.config.get('CAPTCHETAT_OAUTH_BASE_URL'), params={
-            'grant_type': 'client_credentials',
-            'scope': 'WRITE',
-            'client_id': current_app.config.get('CAPTCHETAT_CLIENT_ID'),
-            'client_secret': current_app.config.get('CAPTCHETAT_CLIENT_SECRET')
-        })
-        return self.source.config
+        url = current_app.config.get('CAPTCHETAT_OAUTH_BASE_URL') + '/api/oauth/token'
+        previous_value = cache.get(self.cache_key) or ""
+        print(url)
+        try: 
+            oauth = requests.post(url, data={
+                'grant_type': 'client_credentials',
+                'scope': 'WRITE',
+                'client_id': current_app.config.get('CAPTCHETAT_CLIENT_ID'),
+                'client_secret': current_app.config.get('CAPTCHETAT_CLIENT_SECRET')
+            })
+            body = oauth.json()
+            access_token = body.get('access_token', previous_value)
+            self.token_expired_at = datetime.now() + timedelta(seconds=body.get('expires_in', 0))
+        except requests.exceptions.RequestException as e:
+            log.exception(f'Error while getting access token from {url}')
+            access_token = previous_value
+        else:
+            return access_token
 
     @apiv2.expect(captchetat_parser)
     @apiv2.doc('captchetat')
     def get(self):
         '''CaptchEtat endpoint for captcha generation and validation'''
         args = captchetat_parser.parse_args()
-        headers = {}
-        captchetat_url = current_app.config.get('CAPTCHETAT_BASE_URL')
+        headers = {'Authorization': 'Bearer ' + self.bearer_token()}
+        captchetat_url = current_app.config.get('CAPTCHETAT_BASE_URL') + "/simple-captcha-endpoint"
         req = requests.get(captchetat_url, headers=headers, params=args)
-        return req.status_code
+        return req.text
