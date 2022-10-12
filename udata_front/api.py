d623ffe3ccf3b2550cc4bda4b5a2e6b4fe0a34ad
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-10-12 10:25:14 +0200

    docs: :memo: add argument `help` and update comment

--- udata_front/api.py
+++ udata_front/api.py
@@ -11,15 +11,15 @@ log = logging.getLogger(__name__)
 
 ns = apiv2.namespace('captchetat', 'CaptchEtat related operations')
 captchetat_parser = apiv2.parser()
-captchetat_parser.add_argument('get', type=str, location='args')
-captchetat_parser.add_argument('c', type=str, location='args')
-captchetat_parser.add_argument('d', type=str, location='args')
-captchetat_parser.add_argument('t', type=str, location='args')
-captchetat_parser.add_argument('cs', type=str, location='args')
+captchetat_parser.add_argument('get', type=str, location='args', help='type of data wanted from captchetat')
+captchetat_parser.add_argument('c', type=str, location='args', help='captcha name')
+captchetat_parser.add_argument('d', type=str, location='args', help='this is a technical argument auto-generated for audio content')
+captchetat_parser.add_argument('t', type=str, location='args', help='captcha id, this is a technical argument auto-generated')
+captchetat_parser.add_argument('cs', type=str, location='args', help='captcha style code, this is a technical argument auto-generated')
 
 
 def bearer_token():
-    '''Get CaptchEtat bearer token from cache or get a new one from captchEtat Oauth server'''
+    '''Get CaptchEtat bearer token from cache or get a new one from CaptchEtat Oauth server'''
     token_cache_key = current_app.config.get('TOKEN_CACHE_KEY')
     url = current_app.config.get('CAPTCHETAT_OAUTH_TOKEN_URL')
     previous_value = cache.get(token_cache_key)
