a9f08f1eabe5fa0af59c6f96c5559ad9ce4ba837
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-11-24 16:54:28 +0100

    refactor(i18n): :construction_worker: use vue-i18n-extract config file

--- theme/js/plugins/i18n.js
+++ theme/js/plugins/i18n.js
@@ -7,7 +7,7 @@ import { createI18n } from "vue-i18n";
 // TODO : use glob import from Vite : https://vitejs.dev/guide/features.html#glob-import
 import en from "../locales/en.json";
 import es from "../locales/es.json";
-import fr from "../locales/fr.json"
+import fr from "../locales/fr.json";
 
 
 export default createI18n({
---
+++ vue-i18n-extract.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "vueFiles": "theme/**/*.?(js|vue)",
+  "languageFiles": "theme/js/locales/**/*.?(json|yaml|yml|js)",
+  "exclude": [],
+  "output": false,
+  "add": false,
+  "remove": false,
+  "ci": false,
+  "separator": "^",
+  "noEmptyTranslation": ""
+}
