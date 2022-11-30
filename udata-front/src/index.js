b83a0d5818eba9c58258d02b4833af571ef0d802
Nicolas KEMPF <nicolas@conciergerie.dev>
2022-11-30 10:55:44 +0100

    refactor: :fire: remove hook registration (unused)

--- udata-front/src/index.js
+++ udata-front/src/index.js
@@ -1,12 +1,6 @@
 /**
- * @typedef {object} Hook
- * @property {string} name - Hook name, used to register components (see {@link Component})
- * @property {string} [module] - Udata module registering this hook
- */
-
-/**
  * @typedef {object} Component
- * @property {string} hook - Hook name, used to match this component with an existing {@link Hook}
+ * @property {string} hook - Hook name
  * @property {import("vue").Component} component - Vue Component to show in udata-front
  * @property {string} [module] module - Udata module registering this component, logged before the component is used
  * * @property {string} [name] - Component name, logged before the component is used
@@ -19,19 +13,16 @@
  */
 
 /** @typedef {Object<string, Array<Component>>} Components - see {@link Component} */
-/** @typedef {Array<Hook>} Hooks - see {@link Hook} */
 /** @typedef {Array<Translation>} Translations - see {@link Translation} */
 
 /**
  * @typedef {object} UdataFrontNamespace
- * @property {Hooks} hooks - Hooks defined for this udata-front instance (see {@link Hook})
- * @property {Components} components - Key-value pair matching components (see {@link Component}) with hooks (see {@link Hook})
+ * @property {Components} components - Key-value pair matching components (see {@link Component}) with hooks
  * @property {Translations} translations - Translations to add to vue-i18n (see {@link Translation})
 */
 
 /** @type {UdataFrontNamespace} */
 const udata_front = globalThis.udata_front ?? {
-  hooks: [],
   components: {},
   translations: [],
 }
@@ -63,24 +54,6 @@ export function registerComponent(hook, component, module, name) {
   return udataFrontComponent;
 }
 
-
-/**
- * @callback RegisterHook
- * @param {string} name - Name of the {@link Hook}
- * @param {string} [module] - Udata module, logged when the hook is called
- * @returns {Hook}
- */
-
-/** @type {RegisterHook} */
-export function registerHook(name, module) {
-  const hook = {
-    name,
-    module,
-  };
-  udata_front.hooks.push(hook);
-  return hook;
-}
-
 /**
  * @callback RegisterTranslation
  * @param {object} messages - Name of the {@link Hook}
@@ -109,11 +82,6 @@ export function getRegisteredComponents() {
 */
 export function getRegisteredComponentsForHook(name) {
   return udata_front.components[name] ?? [];
-}
-
-/** @returns {Hooks} */
-export function getRegisteredHooks() {
-  return udata_front.hooks;
 }
 
 /** @returns {Translations} */
