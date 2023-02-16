82112465abc15dd5dd494e299ba0222ae4f4f37b
Nicolas KEMPF <nicolas@conciergerie.dev>
2023-02-16 14:33:02 +0100

    test: :white_check_mark: fix tests

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,33 +1,10 @@
-/// <reference types="cypress" />
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
+const { defineConfig } = require("cypress");
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-
-const axios = require('axios').default;
-
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  on('task', {
-    /**
-     * This task use the sitemap to get pages.
-     * It limits the number of datasets, reuses and organisations visited to 200
-     * This logic should be removed when we use fixtures for testing
-     * instead of the development instance.
-     * @returns {Promise<Array<string>>}
-     */
-    sitemapLocations() {
+module.exports = defineConfig({
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
       console.log('Starting script');
       return getLocations(`${config.baseUrl}/sitemap.xml`).then(locations =>
         Promise.all(locations.map(url => getLocations(url)))
@@ -36,20 +13,32 @@ module.exports = (on, config) => {
             let countDatasets = 0;
             let countReuses = 0;
             let countOrganizations = 0;
-            return urls.filter(url =>
+            config.env.URLS = urls.filter(url =>
               (!url.includes('/datasets/') || ++countDatasets <= 200) &&
               (!url.includes('/reuses/') || ++countReuses <= 200) &&
               (!url.includes('/organizations/') || ++countOrganizations <= 200)
             );
+            return config;
           })
       );
     },
-  })
-}
+    baseUrl: "http://dev.local:7000",
+    numTestsKeptInMemory: 10,
+  },
+
+  component: {
+    devServer: {
+      framework: "vue",
+      bundler: "vite",
+    },
+  },
+});
+
+const axios = require('axios').default;
 
 /**
  * Loads Sitemap and extracts urls
- * @param {string} url 
+ * @param {string} url
  * @returns {Promise<Array<string>>}
  */
 const getLocations = (url) => {
--- cypress/e2e/a11y.cy.js
+++ cypress/e2e/a11y.cy.js
@@ -1,32 +1,31 @@
-it('should be accessible', () => {
-    cy.task('sitemapLocations').then(pages => {
-        pages.forEach(page => {
-            cy.intercept('**/api/**').as('request')
-            cy.intercept('**/_themes/gouvfr/**').as('files')
-            cy.visit(page)
-            cy.injectAxe()
-            cy.wait(['@request', '@files'])
-            cy.checkA11y(
-                {
-                    /**
-                     * This parameter is a array of query selectors that will be excluded from the accessibility check.
-                     * It uses `markdown` class to avoid errors due to user generated content.
-                     */
-                    exclude: ['.markdown'],
-                },
-                {
-                    /**
-                     * This parameter is a dictionary of [Axe rules](https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md) with custom configuration.
-                     * It disables `color-contrast` due to an issue with this rule and `background-image` [3464](https://github.com/dequelabs/axe-core/issues/3464)
-                     * that causes the rule to fail for all DSFR content with `:hover`.
-                     */
-                    rules: {
-                        'color-contrast': { enabled: false },
-                    },
-                },
-                (violations) => console.log(violations),
-                true
-            )
-        })
+describe('should be accessible', () => {
+  Cypress.env('URLS').forEach(page => {
+    it(`Test for page: ${page}`, function() {
+    cy.intercept('**/?(api|_themes)/**').as('request')
+    cy.visit(page)
+    cy.injectAxe()
+    cy.wait(['@request'])
+    cy.checkA11y(
+      {
+        /**
+         * This parameter is a array of query selectors that will be excluded from the accessibility check.
+         * It uses `markdown` class to avoid errors due to user generated content.
+         */
+        exclude: ['.markdown'],
+      },
+      {
+        /**
+         * This parameter is a dictionary of [Axe rules](https://github.com/dequelabs/axe-core/blob/develop/doc/rule-descriptions.md) with custom configuration.
+         * It disables `color-contrast` due to an issue with this rule and `background-image` [3464](https://github.com/dequelabs/axe-core/issues/3464)
+         * that causes the rule to fail for all DSFR content with `:hover`.
+         */
+        rules: {
+          'color-contrast': { enabled: false },
+        },
+      },
+      (violations) => console.log(violations),
+      true
+    );
     })
+  })
 })
