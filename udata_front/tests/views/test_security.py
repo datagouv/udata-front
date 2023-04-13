4b17efc6940f4acca92fa785e7615326ac1b8a6c
maudetes <maudet.estelle@gmail.com>
2023-04-13 10:22:02 +0200

    Fix test linting

--- udata_front/tests/views/test_security.py
+++ udata_front/tests/views/test_security.py
@@ -12,20 +12,20 @@ class SecurityViewsTest(GouvfrFrontTestCase):
         '''Login should redirect to the correct next endpoint: homepage'''
         response = self.get(url_for('site.home'))
         lang = self.app.config['DEFAULT_LANGUAGE']
-        expected = f'href="/{lang}/login?next=%2F{lang}%2F"'.encode('utf-8')
-        assert expected in response.data
+        expected = f'href="/{lang}/login?next=%2F{lang}%2F"'
+        assert expected.encode('utf-8') in response.data
 
     def test_security_login_next_datasets(self):
         '''Login should redirect to the correct next endpoint: dataset'''
         dataset = DatasetFactory(slug="dataset-slug")
         response = self.get(url_for('datasets.show', dataset=dataset))
         lang = self.app.config['DEFAULT_LANGUAGE']
-        expected = f'href="/{lang}/login?next=%2F{lang}%2Fdatasets%2Fdataset-slug%2F"'.encode('utf-8')
-        assert expected in response.data
+        expected = f'href="/{lang}/login?next=%2F{lang}%2Fdatasets%2Fdataset-slug%2F"'
+        assert expected.encode('utf-8') in response.data
 
     def test_security_login_next_exception_site_home(self):
         '''Login should redirect to the homepage if current request has an exception'''
         response = self.get(url_for('datasets.show', dataset='dataset-not-found'))
         lang = self.app.config['DEFAULT_LANGUAGE']
-        expected = f'href="/{lang}/login?next=%2F{lang}%2F"'.encode('utf-8')
-        assert expected in response.data
+        expected = f'href="/{lang}/login?next=%2F{lang}%2F"'
+        assert expected.encode('utf-8') in response.data
