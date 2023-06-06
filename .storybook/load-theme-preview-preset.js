230fd1046c72fc3dc5188b4bd4f68540bdd9d898
Nicolas KEMPF <nicolas@conciergerie.dev>
2023-06-06 15:39:57 +0200

    build: :wrench: storybook theme

--- .storybook/load-theme-preview-preset.js
+++ .storybook/load-theme-preview-preset.js
@@ -7,4 +7,9 @@ export function previewAnnotations(entry = []) {
   return [...entry, require.resolve(`../udata_front/theme/${theme}/assets/js/.storybook/preview.js`)];
 }
 
-export default { previewAnnotations }
+export function managerEntries(entry = []) {
+  const theme = getTheme();
+  return [...entry, require.resolve(`../udata_front/theme/${theme}/assets/js/.storybook/manager.js`)];
+}
+
+export default { managerEntries, previewAnnotations }
