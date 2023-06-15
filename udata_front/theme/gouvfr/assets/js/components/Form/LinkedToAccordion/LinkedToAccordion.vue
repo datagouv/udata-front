b3c618bdd56d241605eb9fb0e08167e6d70bc415
Nicolas KEMPF <nicolas@conciergerie.dev>
2023-06-15 14:02:14 +0200

    feat(publishing): :sparkles: add linked to accordion component

---
+++ udata_front/theme/gouvfr/assets/js/components/Form/LinkedToAccordion/LinkedToAccordion.vue
@@ -0,0 +1,30 @@
+<template>
+  <div @focusin="openIfClosed">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    accordion: {
+      type: String,
+      required: true,
+    }
+  },
+  setup(props) {
+    const openIfClosed = () => {
+      const button = document.querySelector(`[aria-controls=${props.accordion}]`);
+      if(button && button.ariaExpanded !== "true") {
+        button.ariaExpanded = "true";
+      }
+    };
+
+    return {
+      openIfClosed,
+    };
+  },
+});
+</script>
