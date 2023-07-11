22e6c3a5542039d289d823b429e092ec35d1de91
maudetes <maudet.estelle@gmail.com>
2023-07-11 18:09:31 +0200

    Add a redirect when missing trailing slash
    Update existing tests to call slug with trailing slash to prevent redirect

--- udata_front/tests/test_static_pages.py
+++ udata_front/tests/test_static_pages.py
@@ -19,21 +19,21 @@ class StaticPagesTest:
         raw_url, gh_url = get_pages_gh_urls('doesnotexist')
         rmock.head(f'{raw_url}.md', status_code=404)
         rmock.get(f'{raw_url}.html', status_code=404)
-        response = client.get(url_for('gouvfr.show_page', slug='doesnotexist'))
+        response = client.get(url_for('gouvfr.show_page', slug='doesnotexist/'))
         assert response.status_code == 404
 
     def test_page_error_no_cache(self, client, rmock):
         raw_url, gh_url = get_pages_gh_urls('doesnotexist')
         rmock.head(f'{raw_url}.md', status_code=500)
         rmock.get(f'{raw_url}.html', status_code=500)
-        response = client.get(url_for('gouvfr.show_page', slug='doesnotexist'))
+        response = client.get(url_for('gouvfr.show_page', slug='doesnotexist/'))
         assert response.status_code == 503
 
     def test_page_timeout_no_cache(self, client, rmock):
         raw_url, gh_url = get_pages_gh_urls('doesnotexist')
         rmock.head(f'{raw_url}.md', status_code=404)
         rmock.get(f'{raw_url}.html', exc=requests.exceptions.ConnectTimeout)
-        response = client.get(url_for('gouvfr.show_page', slug='doesnotexist'))
+        response = client.get(url_for('gouvfr.show_page', slug='doesnotexist/'))
         assert response.status_code == 503
 
     def test_page_error_w_cache(self, client, rmock, mocker):
@@ -43,11 +43,11 @@ class StaticPagesTest:
         # fill cache
         rmock.head(f'{raw_url}.md', status_code=200)
         rmock.get(f'{raw_url}.md', text="""#test""")
-        response = client.get(url_for('gouvfr.show_page', slug='cache1'))
+        response = client.get(url_for('gouvfr.show_page', slug='cache1/'))
         assert cache_mock_set.called
         rmock.head(f'{raw_url}.md', status_code=500)
         rmock.get(f'{raw_url}.html', status_code=500)
-        response = client.get(url_for('gouvfr.show_page', slug='cache1'))
+        response = client.get(url_for('gouvfr.show_page', slug='cache1/'))
         assert response.status_code == 200
         assert b'dummy_from_cache' in response.data
         assert rmock.call_count == 4
@@ -57,7 +57,7 @@ class StaticPagesTest:
         raw_url, _ = get_pages_gh_urls('cache1')
         rmock.head(f'{raw_url}.md', status_code=500)
         rmock.get(f'{raw_url}.html', status_code=500)
-        response = client.get(url_for('gouvfr.show_page', slug='cache1'))
+        response = client.get(url_for('gouvfr.show_page', slug='cache1/'))
         assert response.status_code == 503
 
     def test_page_extension_detection_md(self, client, rmock):
@@ -76,7 +76,7 @@ class StaticPagesTest:
         raw_url, _ = get_pages_gh_urls('test')
         rmock.head(f'{raw_url}.md', status_code=200)
         rmock.get(f'{raw_url}.md', text="""#test""")
-        response = client.get(url_for('gouvfr.show_page', slug='test'))
+        response = client.get(url_for('gouvfr.show_page', slug='test/'))
         assert response.status_code == 200
         assert b'<h1>test</h1>' in response.data
 
@@ -84,7 +84,7 @@ class StaticPagesTest:
         raw_url, _ = get_pages_gh_urls('test')
         rmock.head(f'{raw_url}.md', status_code=404)
         rmock.get(f'{raw_url}.html', text="""<h1>test</h1>""")
-        response = client.get(url_for('gouvfr.show_page', slug='test'))
+        response = client.get(url_for('gouvfr.show_page', slug='test/'))
         assert response.status_code == 200
         assert b'<h1>test</h1>' in response.data
 
@@ -97,7 +97,7 @@ reuses:
 ---
 #test
 """)
-        response = client.get(url_for('gouvfr.show_page', slug='test'))
+        response = client.get(url_for('gouvfr.show_page', slug='test/'))
         assert response.status_code == 200
 
     def test_page_inject_objects(self, client, rmock):
@@ -113,7 +113,7 @@ reuses:
 ---
 #test
 """)
-        response = client.get(url_for('gouvfr.show_page', slug='test'))
+        response = client.get(url_for('gouvfr.show_page', slug='test/'))
         assert response.status_code == 200
         assert str(dataset.title).encode('utf-8') in response.data
         assert str(url_for('reuses.show', reuse=reuse)).encode('utf-8') in response.data
@@ -122,7 +122,7 @@ reuses:
         raw_url, _ = get_pages_gh_urls('subdir/test')
         rmock.head(f'{raw_url}.md', status_code=200)
         rmock.get(f'{raw_url}.md', text="""#test""")
-        response = client.get(url_for('gouvfr.show_page', slug='subdir/test'))
+        response = client.get(url_for('gouvfr.show_page', slug='subdir/test/'))
         assert response.status_code == 200
         assert b'<h1>test</h1>' in response.data
 
