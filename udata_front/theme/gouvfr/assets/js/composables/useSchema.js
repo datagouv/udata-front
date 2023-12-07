60e26624bbf0b5cba17a858744574a8a48cbbc35
Nicolas KEMPF <nicolas@conciergerie.dev>
2023-12-07 14:38:59 +0100

    feat: :sparkles: add header-case to validation url

--- udata_front/theme/gouvfr/assets/js/composables/useSchema.js
+++ udata_front/theme/gouvfr/assets/js/composables/useSchema.js
@@ -47,6 +47,7 @@ export default function useSchema(resource) {
     const query = new URLSearchParams({
       'input': 'url',
       'url': resource.url,
+      'header-case': 'on',
       ...schemaPath,
     }).toString();
     return `${config.schema_validata_url}/table-schema?${query}`;
