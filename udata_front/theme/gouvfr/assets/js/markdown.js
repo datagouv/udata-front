f1b4c881b060f8aae9445c5bd9facb5a8da2b8f4
Nicolas KEMPF <nicolas@conciergerie.dev>
2023-12-22 13:53:41 +0100

    feat: :zap: lazy load markdown img

--- udata_front/theme/gouvfr/assets/js/markdown.js
+++ udata_front/theme/gouvfr/assets/js/markdown.js
@@ -8,9 +8,10 @@ const markdown = markdownit({
 });
 
 // Disable mail linkification
-markdown.linkify.add('mailto:', null)
+markdown.linkify.set({ fuzzyEmail: false });
 
 markdown.use(function(md) {
+    // Add `rel="ugc nofollow"` to links
     md.renderer.rules.link_open = function(tokens, idx, options, env, self) {
         const link_open = tokens[idx];
         link_open.attrs.push(['rel','ugc nofollow']);
@@ -28,6 +29,14 @@ markdown.use(function(md) {
         s_close.type = 'del_close';
         s_close.tag = 'del';
         return self.renderToken(tokens, idx, options);
+    };
+    // Add `loading="lazy"` to all images
+    md.renderer.rules.image = function (tokens, idx, options, env, slf) {
+      var token = tokens[idx];
+      token.attrs[token.attrIndex('alt')][1] = slf.renderInlineAsText(token.children, options, env);
+      // this is the line of code responsible for an additional 'loading' attribute
+      token.attrSet('loading', 'lazy');
+      return slf.renderToken(tokens, idx, options);
     };
 });
 
