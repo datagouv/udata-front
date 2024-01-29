c294e6a13513bf54e811974550c5a542cedd5576
Nicolas KEMPF <nicolas@conciergerie.dev>
2024-01-29 10:28:16 +0100

    ci: :green_heart: test CI with tag

--- udata_front/__init__.py
+++ udata_front/__init__.py
@@ -2,7 +2,7 @@
 udata customizations for data.gouv.fr
 '''
 
-__version__ = '3.5.1.dev'
+__version__ = '3.5.1.test'
 __description__ = 'udata customizations for data.gouv.fr'
 
 APIGOUVFR_EXTRAS_KEY = 'apigouvfr:apis'
