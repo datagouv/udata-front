0f5656e9126da416e194b0ae7b852714877f7745
Nicolas KEMPF <nicolas@conciergerie.dev>
2024-02-19 14:15:41 +0100

    chore(storybook): :wrench: setup vue-router for storybook

--- udata_front/theme/gouvfr/assets/js/.storybook/preview.ts
+++ udata_front/theme/gouvfr/assets/js/.storybook/preview.ts
@@ -5,10 +5,27 @@ import Toaster from "@conciergerie.dev/vue-toaster";
 import { setup } from '@storybook/vue3';
 import i18n from "../i18n";
 import Api from "../plugins/api";
+import Container from "../components/Ui/Container/Container.vue";
+import { type RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
 
 setup((app) => {
   app.use(Api);
   app.use(i18n);
+
+
+  const routes: Array<RouteRecordRaw> = [
+    { path: '/', component: Container },
+    { path: '/datasets', component: Container },
+    { path: '/reuses', component: Container },
+  ];
+
+  const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+  });
+
+  app.use(router);
+
   app.use(Toaster, {
     duration: false,
     dismissible: true,
