42aa2e5938bbdddd9c9a1a22b00e7143289cb9f8
Kevin Cluzel <kevin.cluzel@hotmail.fr>
2024-03-04 12:05:58 +0100

    feat: create reuseLoader and Step3 of reuse publishing form

---
+++ udata_front/theme/gouvfr/assets/js/pages/ReusePublishingForm/Step3CompleteThePublication.stories.ts
@@ -0,0 +1,73 @@
+import Step3CompleteThePublication from './Step3CompleteThePublication.vue';
+import * as Stepper from '../../components/Form/Stepper/Stepper.stories';
+
+
+export default {
+  title: 'Pages/ReusePublishingForm/Step3',
+  component: Step3CompleteThePublication,
+};
+
+const originalReuse = {
+  badges: [],
+  created_at: "2024-03-03T19:04:43.599000+00:00",
+  datasets: [],
+  deleted: null,
+  description: "This is a random description",
+  extras: {},
+  featured: null,
+  id: "someId",
+  image: "https://picsum.photos/200",
+  image_thumbnail: "https://picsum.photos/200",
+  last_modified: "2024-03-04T08:07:50.215000+00:00",
+  metrics: {
+    datasets: 1,
+    discussions: 0,
+    followers: 0,
+    views: 0
+  },
+  organization: null,
+  owner: {
+    avatar: "https://static.data.gouv.fr/avatars/27/62003f06b04fa996d1e3ae84adbaf9-original.png",
+    avatar_thumbnail: "https://static.data.gouv.fr/avatars/27/62003f06b04fa996d1e3ae84adbaf9-500.png",
+    class: "User",
+    first_name: "Benoit",
+    id: "5bdb10308b4c4108ae679f01",
+    last_name: "Demaegdt",
+    page: "https://www.data.gouv.fr/fr/users/benoit-demaegdt/",
+    slug: "benoit-demaegdt",
+    uri: "https://www.data.gouv.fr/api/1/users/benoit-demaegdt/"
+  },
+  page: "",
+  private: false,
+  slug: "testReuse",
+  tags: [
+    "mobilite",
+    "mobilite-douce",
+    "velo"
+  ],
+  title: "Test Reuse",
+  topic: "transport_and_mobility",
+  type: "vizualization",
+  uri: "",
+  url: "data.gouv.fr"
+};
+
+const args = {
+  originalReuse,
+  feedbackUrl: "https://demo.data.gouv.fr/fr/datasets/?q=feedback",
+  steps: Stepper.StepperOnSecondStep.args.steps,
+  redirectDraftUrl: "https://demo.data.gouv.fr/fr/datasets/?q=draft",
+};
+
+export const Step3 = {
+  render: (args) => ({
+    components: { Step3CompleteThePublication },
+    setup() {
+      return { args };
+    },
+    template: ` <div class="bg-grey-50 fr-p-4w">
+                  <Step3CompleteThePublication v-bind="args" />
+                </div>`,
+  }),
+  args,
+};
---
+++ udata_front/theme/gouvfr/assets/js/pages/ReusePublishingForm/Step3CompleteThePublication.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="fr-container">
+      <Stepper :steps="steps" :currentStep="2"/>
+      <Container>
+        <Well
+          color="blue-cumulus"
+          weight="regular"
+          class="fr-mb-2v"
+        >
+          <div class="fr-grid-row">
+            <div class="fr-col-auto fr-mr-3v">
+              <img :src="successIcon" alt="" />
+            </div>
+            <div class="fr-col">
+              <p class="fr-m-0 fr-text--bold">{{ $t('Your reuse is created !') }}</p>
+              <p class="fr-m-0 fr-text--xs">{{ $t('You can now publish it or save it as a draft.') }}</p>
+            </div>
+          </div>
+        </Well>
+        <ul ref="listRef" class="fr-grid-row fr-grid-row--gutters fr-mb-2v">
+          <li
+            class="fr-col-12 fr-col-md-4"
+          >
+            <Loader />
+          </li>
+          <li
+            class="fr-col-12 fr-col-md-4"
+          >
+            <ReuseCard :reuse="reuse" />
+          </li>
+          <li
+            class="fr-col-12 fr-col-md-4"
+          >
+            <Loader />
+          </li>
+        </ul>
+        <div class="fr-grid-row justify-between">
+          <a
+              :href="feedbackUrl"
+              class="fr-btn fr-btn--tertiary-no-outline fr-btn--secondary-grey-500 fr-btn--icon-left fr-icon-lightbulb-line"
+          >
+              {{ $t('Give us your feedback on the publishing form') }}
+          </a>
+          <div class="fr-grid-row fr-grid-row--right">
+            <a :href="redirectDraftUrl" class="fr-btn fr-btn--secondary fr-btn--secondary-grey-500 fr-mr-3v">
+              {{ $t("Save as draft") }}
+            </a>
+            <button @click="submit" class="fr-btn">
+              {{ $t("Publish the reuse") }}
+            </button>
+          </div>
+        </div>
+      </Container>
+    </div>
+  </template>
+  
+  <script>
+  import { defineComponent, reactive } from 'vue';
+  import Container from '../../components/Ui/Container/Container.vue';
+  import Stepper from '../../components/Form/Stepper/Stepper.vue';
+  import Well from "../../components/Ui/Well/Well.vue";
+  import successIcon from "../../../../templates/svg/illustrations/success.svg";
+  import ReuseCard from '../../components/Reuse/Reuse.vue';
+  import Loader from '../../components/Reuse/reuseLoader.vue';
+  
+  export default defineComponent({
+    components: { Container, Stepper, Well, ReuseCard, Loader },
+    emits: ["redirectToPublicUrl"],
+    props: {
+      feedbackUrl: {
+        type: String,
+        required: true,
+      },
+      originalReuse: {
+        /** @type {import("vue").PropType<import("../../types").Reuse>} */
+        type: Object,
+        required: true
+      },
+      redirectDraftUrl: {
+        type: String,
+        required: true,
+      },
+      steps: {
+        type: Array,
+        required: true,
+      },
+    },
+    setup(props, { emit }) {
+      const reuse = reactive({...props.originalReuse, private: false});
+      const submit = () => {
+        emit("redirectToPublicUrl");
+      };
+  
+      return {
+        reuse,
+        submit,
+        successIcon,
+      };
+    },
+  });
+  </script>
+  
