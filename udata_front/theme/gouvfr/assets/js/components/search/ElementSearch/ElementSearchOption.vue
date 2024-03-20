75aca5e1a72b96380f61eefecbbf2a45aab96a4b
Kevin Cluzel <kevin.cluzel@hotmail.fr>
2024-03-20 15:30:08 +0100

    fix: wrong margins corrected

--- udata_front/theme/gouvfr/assets/js/components/search/ElementSearch/ElementSearchOption.vue
+++ udata_front/theme/gouvfr/assets/js/components/search/ElementSearch/ElementSearchOption.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--center">
-    <div class="fr-col fr-display-flex justify-center">
+    <!-- Logo/Placeholder column -->
+    <div class="fr-col-auto fr-display-flex justify-center fr-my-auto">
       <img v-if="logo" :src="logo" alt="Organization Logo" height="50" class="fr-mx-1w" />
-      <Placeholder
-        v-else
-        type="organization"
-        :size="50"
-        class="fr-mx-1w"
-      />
+      <Placeholder v-else type="organization" :size="50" class="fr-mx-1w" />
+    </div>
+    <!-- Name column -->
+    <div class="fr-col fr-display-flex align-items-center fr-my-auto">
       <span class="fr-name">{{ name }}</span>
     </div>
-    <div class="fr-col-auto fr-display-flex text-align-right justify-center fr-my-auto">
+    <!-- Link column -->
+    <div class="fr-col-auto fr-display-flex align-items-center justify-end fr-my-auto">
       <a target="_blank" :href="link">{{ t('See Organization') }}</a>
     </div>
   </div>
@@ -18,7 +18,7 @@
 
   
 <script setup lang="ts">
-import { computed, defineProps } from 'vue';
+import { defineProps } from 'vue';
 import { useI18n } from 'vue-i18n';
 import Placeholder from '../../utils/placeholder.vue';
 const { t } = useI18n();
