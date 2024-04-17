2f40df8403685161504efb964e522e946a20e6b7
Kevin Cluzel <kevin.cluzel@hotmail.fr>
2024-04-17 15:20:32 +0200

    feat: add story for DatasetCard

--- udata_front/theme/gouvfr/datagouv-components/src/components/Avatar/Avatar.stories.ts
+++ udata_front/theme/gouvfr/datagouv-components/src/components/Avatar/Avatar.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
-import Avatar from "./Avatar.vue";
+import { Avatar } from ".";
 import type { User } from "../../types/users";
 
 const meta = {
@@ -13,22 +13,13 @@ const args = {
   user: {
     id: "someUserId",
     first_name: "John",
-    last_name: "Doe"
-  } as User,
-};
-
-
-const userWithAvatarArgs = {
-  user: {
-    id: "someUserId",
-    first_name: "John",
     last_name: "Doe",
     avatar: "https://demo-static.data.gouv.fr/avatars/92/6284d057ca4279af35e9b62bc62639-100.png",
     avatar_thumbnail: "https://demo-static.data.gouv.fr/avatars/92/6284d057ca4279af35e9b62bc62639-100.png",
   } as User,
 };
 
-export const DefaultIdenticonAvatar: StoryObj<typeof meta> = {
+export const UserAvatar: StoryObj<typeof meta> = {
   render: (args) => ({
     components: { Avatar },
     setup() {
@@ -37,16 +28,4 @@ export const DefaultIdenticonAvatar: StoryObj<typeof meta> = {
     template: ` <Avatar v-bind="args"/>`,
   }),
   args,
-};
-
-
-export const StaticAvatar: StoryObj<typeof meta> = {
-  render: (args) => ({
-    components: { Avatar },
-    setup() {
-      return { args };
-    },
-    template: ` <Avatar v-bind="args"/>`,
-  }),
-  args: userWithAvatarArgs,
 };
