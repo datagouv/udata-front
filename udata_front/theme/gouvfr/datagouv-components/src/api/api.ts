6807130faa43fdafdd3fde251bf6a84730e7e336
Nicolas KEMPF <nicolas@conciergerie.dev>
2024-04-23 14:29:06 +0200

    Fix api calling dev (#401)
    * feat(datagouv-components): :label: add missing env file
    
    * feat(datagouv-components): :label: Type config
    
    * fix(datagouv-components): :ambulance: fix api url not updating on config updates
    
    * fix: :arrow_up: update data.gouv.fr-components to fix api url issues

--- udata_front/theme/gouvfr/datagouv-components/src/api/api.ts
+++ udata_front/theme/gouvfr/datagouv-components/src/api/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { config } from "../config";
+import { computed, watch } from "vue";
 
 const CancelToken = axios.CancelToken;
 export const generateCancelToken = () => CancelToken.source();
@@ -10,9 +11,11 @@ export const generateAbortcontroller = (timeoutMs: number) => {
   return abortController;
 };
 
-export const api_root_absolute = config.api_root?.startsWith("http") ? config.api_root : document.location.origin + config.api_root;
+export const apiRootAbsoluteRef = computed(() => config.api_root?.startsWith("http") ? config.api_root : document.location.origin + config.api_root);
+export let api_root_absolute = apiRootAbsoluteRef.value;
 
-export const api_2_root_absolute = config.api_2_root?.startsWith("http") ? config.api_2_root : document.location.origin + config.api_2_root;
+export const api2RootAbsoluteRef = computed(() => config.api_2_root?.startsWith("http") ? config.api_2_root : document.location.origin + config.api_2_root);
+export let api_2_root_absolute = api2RootAbsoluteRef.value;
 
 // Instantiate axios with base URL from config
 // No need for CSRF or anything fancy here
@@ -20,6 +23,17 @@ export const api_2_root_absolute = config.api_2_root?.startsWith("http") ? confi
 export const api = axios.create({
   baseURL: api_root_absolute,
 });
+
 export const apiv2 = axios.create({
   baseURL: api_2_root_absolute,
+});
+
+watch(apiRootAbsoluteRef, (value) => {
+  api_root_absolute = value;
+  api.defaults.baseURL = value;
+});
+
+watch(api2RootAbsoluteRef, (value) => {
+  api_2_root_absolute = value;
+  apiv2.defaults.baseURL = value;
 });
