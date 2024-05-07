794d47d431b1c06485ce08aa2e48b555d54066eb
Kevin Cluzel <kevin.cluzel@hotmail.fr>
2024-05-07 13:04:17 +0200

    fix: edit OrganizationSearchOption design to match figma

--- udata_front/theme/gouvfr/assets/js/components/search/OrganizationSearch/OrganizationSearchOption.vue
+++ udata_front/theme/gouvfr/assets/js/components/search/OrganizationSearch/OrganizationSearchOption.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--center">
+  <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--center border-default-grey border-bottom">
     <!-- Logo/Placeholder column -->
     <div class="fr-col-auto fr-display-flex justify-center fr-my-auto">
-      <img v-if="logo" :src="logo" alt="Organization Logo" height="50" class="fr-mx-1w" />
-      <Placeholder v-else type="organization" :size="50" class="fr-mx-1w" />
+      <img v-if="logo" :src="logo" alt="Organization Logo" height="30" class="fr-mx-1w" />
+      <Placeholder v-else type="organization" :size="30" class="fr-mx-1w" />
     </div>
     <!-- Name column -->
     <div class="fr-col fr-display-flex align-items-center fr-my-auto">
       <span class="fr-name">{{ name }}</span>
     </div>
     <!-- Link column -->
-    <div class="fr-col-auto fr-display-flex align-items-center justify-end fr-my-auto">
+    <div class="fr-col-auto fr-display-flex align-items-center justify-end fr-my-auto fr-mr-1w">
       <a target="_blank" :href="link">{{ t('See Organization') }}</a>
     </div>
   </div>
