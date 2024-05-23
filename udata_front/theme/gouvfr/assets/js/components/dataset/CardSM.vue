e12d9cbb61f0cf4c0f394407f15d4baa661c0a40
Kevin Cluzel <kevin.cluzel@hotmail.fr>
2024-05-23 14:59:23 +0200

    fix: minor fixes

--- udata_front/theme/gouvfr/assets/js/components/dataset/CardSM.vue
+++ udata_front/theme/gouvfr/assets/js/components/dataset/CardSM.vue
@@ -1,129 +1,129 @@
 <template>
-    <article class="fr-my-2w fr-p-2w border border-default-grey fr-enlarge-link" :style="style">
-      <div class="absolute top-0 fr-grid-row fr-grid-row--middle fr-mt-n3v" v-if="dataset.private || dataset.archived">
-        <p class="fr-badge fr-badge--mention-grey fr-mr-1w" v-if="dataset.private">
-          <span class="fr-icon-lock-line" aria-hidden="true"></span>
-          {{ t('Private') }}
-        </p>
-        <p class="fr-badge fr-badge--mention-grey" v-if="dataset.archived">
-          <span class="fr-icon-archive-line" aria-hidden="true"></span>
-          {{ t('Archived') }}
-        </p>
-      </div>
-      <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--top">
-        <div class="fr-col-auto">
-          <div class="logo">
-            <Placeholder
-              v-if="dataset.organization"
-              type="dataset"
-              :src="dataset.organization.logo_thumbnail"
-              alt=""
-              :size="60"
-            />
-            <Avatar
-              v-else-if="dataset.owner"
-              :user="dataset.owner"
-              :size="60"
-            />
-            <Placeholder
-              v-else
-              type="dataset"
-              :size="60"
-            />
-          </div>
+  <article class="fr-my-2w fr-p-2w border border-default-grey fr-enlarge-link" :style="style">
+    <div class="absolute top-0 fr-grid-row fr-grid-row--middle fr-mt-n3v" v-if="dataset.private || dataset.archived">
+      <p class="fr-badge fr-badge--mention-grey fr-mr-1w" v-if="dataset.private">
+        <span class="fr-icon-lock-line" aria-hidden="true"></span>
+        {{ t('Private') }}
+      </p>
+      <p class="fr-badge fr-badge--mention-grey" v-if="dataset.archived">
+        <span class="fr-icon-archive-line" aria-hidden="true"></span>
+        {{ t('Archived') }}
+      </p>
+    </div>
+    <div class="fr-grid-row fr-grid-row--gutters fr-grid-row--top">
+      <div class="fr-col-auto">
+        <div class="logo">
+          <Placeholder
+            v-if="dataset.organization"
+            type="dataset"
+            :src="dataset.organization.logo_thumbnail"
+            alt=""
+            :size="60"
+          />
+          <Avatar
+            v-else-if="dataset.owner"
+            :user="dataset.owner"
+            :size="60"
+          />
+          <Placeholder
+            v-else
+            type="dataset"
+            :size="60"
+          />
         </div>
-        <div class="fr-col">
-          <h4 class="fr-mb-1v fr-grid-row">
-            <a :href="dataset.page" class="text-grey-500">
-              {{ dataset.title }}
-              <small v-if="dataset.acronym">{{ dataset.acronym }}</small>
+      </div>
+      <div class="fr-col">
+        <h4 class="fr-mb-1v fr-grid-row">
+          <a :href="dataset.page" class="text-grey-500">
+            {{ dataset.title }}
+            <small v-if="dataset.acronym">{{ dataset.acronym }}</small>
+          </a>
+        </h4>
+        <p class="fr-my-0 fr-text--xs " v-if="dataset.organization || dataset.owner">
+          <span class="not-enlarged fr-mr-1v" v-if="dataset.organization">
+            <a class="fr-link fr-text--xs" :href="dataset.organization.page">
+              <OrganizationNameWithCertificate :organization="dataset.organization" />
             </a>
-          </h4>
-          <p class="fr-my-0 fr-text--xs " v-if="dataset.organization || dataset.owner">
-            <span class="not-enlarged fr-mr-1v" v-if="dataset.organization">
-              <a class="fr-link fr-text--xs" :href="dataset.organization.page">
-                <OrganizationNameWithCertificate :organization="dataset.organization" />
-              </a>
-            </span>
-            <span class="not-enlarged fr-mr-1v" v-else-if="dataset.owner">
-              <a class="fr-link fr-text--xs" :href="dataset.owner.page">
-                {{ ownerName }}
-              </a>
-            </span>
-            <span class="text-mention-grey">{{ $t('Updated {date}', {date: formatRelativeIfRecentDate(dataset.last_update)}) }}</span>
-          </p>
-          <div class="fr-mx-0 fr-mt-n2v fr-grid-row fr-grid-row--middle fr-text--sm text-mention-grey">
-            <div class="fr-grid-row fr-grid-row--middle fr-hidden flex-sm dash-after text-grey-500 not-enlarged">
-              <p class="fr-m-0 fr-mr-1v text-mention-grey fr-text--sm" :id="id">
-                {{$t('Metadata quality:')}}
-              </p>
-              <div class="fr-grid-row fr-grid-row--middle fr-mr-1v">
-                <QualityScore :score="dataset.quality.score"/>
-              </div>
-            </div>
+          </span>
+          <span class="not-enlarged fr-mr-1v" v-else-if="dataset.owner">
+            <a class="fr-link fr-text--xs" :href="dataset.owner.page">
+              {{ ownerName }}
+            </a>
+          </span>
+          <span class="text-mention-grey">{{ $t('Updated {date}', {date: formatRelativeIfRecentDate(dataset.last_update)}) }}</span>
+        </p>
+        <div class="fr-mx-0 fr-mt-n2v fr-grid-row fr-grid-row--middle fr-text--sm text-mention-grey">
+          <div class="fr-grid-row fr-grid-row--middle fr-hidden flex-sm dash-after text-grey-500 not-enlarged">
+            <p class="fr-m-0 fr-mr-1v text-mention-grey fr-text--sm" :id="id">
+              {{$t('Metadata quality:')}}
+            </p>
             <div class="fr-grid-row fr-grid-row--middle fr-mr-1v">
-              <p class="fr-text--xs"><span class="fr-icon-download-line fr-px-1v" aria-hidden="true"></span>{{ dataset.metrics.discussions }}</p>
-              <p class="fr-text--xs"><span class="fr-icon-star-line fr-px-1v" aria-hidden="true"></span>{{ dataset.metrics.followers }}</p>
-              <p class="fr-text--xs"><span class="fr-icon-line-chart-line fr-px-1v" aria-hidden="true"></span>{{ dataset.metrics.views }}</p>
+              <QualityScore :score="dataset.quality.score"/>
             </div>
+          </div>
+          <div class="fr-grid-row fr-grid-row--middle fr-mr-1v">
+            <p class="fr-text--xs"><span class="fr-icon-download-line fr-px-1v" aria-hidden="true"></span>{{ dataset.metrics.discussions }}</p>
+            <p class="fr-text--xs"><span class="fr-icon-star-line fr-px-1v" aria-hidden="true"></span>{{ dataset.metrics.followers }}</p>
+            <p class="fr-text--xs"><span class="fr-icon-line-chart-line fr-px-1v" aria-hidden="true"></span>{{ dataset.metrics.views }}</p>
           </div>
         </div>
       </div>
-    </article>
-  </template>
-  
-  <script lang="ts">
-  import { defineComponent, PropType } from "vue";
-  import { useI18n } from "vue-i18n";
-  import useLicense from "../../composables/useLicense";
-  import { OrganizationNameWithCertificate, useOwnerName, formatRelativeIfRecentDate} from "@etalab/data.gouv.fr-components";
-  import useUid from "../../composables/useUid";
-  import Avatar from "../discussions/Avatar/Avatar.vue";
-  import Placeholder from "../utils/placeholder.vue";
-  import { QualityItem, QualityScore, Toggletip } from '@etalab/data.gouv.fr-components';
-  import { excerpt } from "../../helpers";
-  import { guides_quality_url } from "../../config";
-  import type { Dataset } from "../../types";
-  
-  export default defineComponent({
-    components: {
-      Avatar,
-      OrganizationNameWithCertificate,
-      Placeholder,
-      QualityScore,
-      Toggletip,
-      QualityItem,
-  },
-    inheritAttrs: false,
-    props: {
-      dataset: {
-        type: Object as PropType<Dataset>,
-        required: true,
-      },
-      style: {
-        type: Object,
-        default: () => ({})
-      },
-      showMetrics: {
-        type: Boolean,
-        default: true,
-      },
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import { useI18n } from "vue-i18n";
+import useLicense from "../../composables/useLicense";
+import { OrganizationNameWithCertificate, useOwnerName, formatRelativeIfRecentDate} from "@etalab/data.gouv.fr-components";
+import useUid from "../../composables/useUid";
+import Avatar from "../discussions/Avatar/Avatar.vue";
+import Placeholder from "../utils/placeholder.vue";
+import { QualityItem, QualityScore, Toggletip } from '@etalab/data.gouv.fr-components';
+import { excerpt } from "../../helpers";
+import { guides_quality_url } from "../../config";
+import type { Dataset } from "../../types";
+
+export default defineComponent({
+  components: {
+    Avatar,
+    OrganizationNameWithCertificate,
+    Placeholder,
+    QualityScore,
+    Toggletip,
+    QualityItem,
+},
+  inheritAttrs: false,
+  props: {
+    dataset: {
+      type: Object as PropType<Dataset>,
+      required: true,
     },
-    setup(props) {
-      const { t } = useI18n();
-      const { id } = useUid("metadata-quality");
-      const ownerName = useOwnerName(props.dataset);
-      const license = useLicense(props.dataset.license);
-      return {
-        excerpt,
-        formatRelativeIfRecentDate,
-        guides_quality_url,
-        license,
-        id,
-        ownerName,
-        t,
-      };
-    }
-  });
-  </script>
+    style: {
+      type: Object,
+      default: () => ({})
+    },
+    showMetrics: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  setup(props) {
+    const { t } = useI18n();
+    const { id } = useUid("metadata-quality");
+    const ownerName = useOwnerName(props.dataset);
+    const license = useLicense(props.dataset.license);
+    return {
+      excerpt,
+      formatRelativeIfRecentDate,
+      guides_quality_url,
+      license,
+      id,
+      ownerName,
+      t,
+    };
+  }
+});
+</script>
   
--- udata_front/theme/gouvfr/assets/js/pages/ReusePublishingForm/Step2AddDatasets.vue
+++ udata_front/theme/gouvfr/assets/js/pages/ReusePublishingForm/Step2AddDatasets.vue
@@ -1,78 +1,74 @@
 <template>
-  <Container class="fr-container">
-    <div class="fr-grid-row">
-        <Container>
-          <Well
-            color="blue-cumulus"
-            weight="regular"
-            class="fr-mb-2w"
-          >
-            <div class="fr-grid-row">
-              <div class="fr-col-auto fr-mr-3v">
-                <!--<img :src="reuseIcon" alt="" />-->
-              </div>
-              <div class="fr-col">
-                <p class="fr-m-0 fr-text--bold">{{ $t('What is a reuse?') }}</p>
-                <p class="fr-m-0 fr-text--xs">{{ $t('A reuse is an exemple of public data\'s usage.') }} {{ $t('Publishing a reuse can allow you to gain visibility and start start a dialogue with the dataset producer.') }}</p>
-              </div>
-            </div>
-          </Well>
-          <fieldset class="fr-fieldset min-width-0" aria-labelledby="description-legend">
-            <legend class="fr-fieldset__legend" id="description-legend">
-              <h2 class="subtitle subtitle--uppercase fr-mb-3v">
-                {{ $t("Associated datasets") }}
-              </h2>
-            </legend>
-            <div class="fr-grid-row fr-grid-row--center w-100" v-for="(dataset, index) in datasets" :key="dataset.id">
-              <div class="fr-col">
-                <CardSm
-                  :dataset="dataset"
-                />
-              </div>
-              <div class="fr-col-1 fr-my-auto fr-ml-auto justify-center flex">
-                <button
-                  type="button"
-                  class="fr-btn fr-btn--sm fr-btn--tertiary fr-icon-delete-line"
-                  @click="removeDataset(index)"
-                ></button>
-              </div>
-            </div>
-            <MultiSelect
-              :minimumCharacterBeforeSuggest="2"
-              @change="addDataset"
-              :placeholder="$t('Look for a dataset')"
-              :searchPlaceholder="$t('Search a dataset...')"
-              suggestUrl="/datasets/suggest/"
-            />
-            <p class="fr-hr-or w-100 text-transform-lowercase fr-text--regular">
-              <span class="fr-hr-or-text">{{ $t('or') }}</span>
-            </p>
-            <InputGroup
-              :label="$t('Link to the dataset')"
-              :placeholder="'https://...'"
-              class="w-100"
-              v-model="linkedDataset"
-              @change="getLinkedDataset"
-            />
-            <div v-if="datasetNotFound" class="fr-col bg-contrast-grey text-align-center fr-p-2v">
-              <p class="fr-text--md fr-text--bold">{{ t('No dataset associated to that link has been found') }}</p>
-            </div>
-          </fieldset>
-          <Alert type="error" v-if="errors.length" class="fr-mt-n2w fr-mb-2w">
-            <template #title>{{ t("An error occured | Some errors occured", errors.length) }}</template>
-            <ul v-if="errors.length > 1">
-              <li v-for="error in errors">{{ error }}</li>
-            </ul>
-            <p v-else> {{ errors[0] }}</p>
-          </Alert>
-          <div class="fr-grid-row fr-grid-row--right">
-            <button class="fr-btn" @click="submit">
-              {{ $t("Next") }}
-            </button>
+    <Container>
+      <Well
+        color="blue-cumulus"
+        weight="regular"
+        class="fr-mb-2w"
+      >
+        <div class="fr-grid-row">
+          <div class="fr-col-auto fr-mr-3v">
+            <!--<img :src="reuseIcon" alt="" />-->
           </div>
-        </Container>
+          <div class="fr-col">
+            <p class="fr-m-0 fr-text--bold">{{ $t('What is a reuse?') }}</p>
+            <p class="fr-m-0 fr-text--xs">{{ $t('A reuse is an exemple of public data\'s usage.') }} {{ $t('Publishing a reuse can allow you to gain visibility and start start a dialogue with the dataset producer.') }}</p>
+          </div>
+        </div>
+      </Well>
+      <fieldset class="fr-fieldset min-width-0" aria-labelledby="description-legend">
+        <legend class="fr-fieldset__legend" id="description-legend">
+          <h2 class="subtitle subtitle--uppercase fr-mb-3v">
+            {{ $t("Associated datasets") }}
+          </h2>
+        </legend>
+        <div class="fr-grid-row fr-grid-row--center w-100" v-for="(dataset, index) in datasets" :key="dataset.id">
+          <div class="fr-col">
+            <CardSm
+              :dataset="dataset"
+            />
+          </div>
+          <div class="fr-col-1 fr-my-auto fr-ml-auto justify-center flex">
+            <button
+              type="button"
+              class="fr-btn fr-btn--sm fr-btn--tertiary fr-icon-delete-line"
+              @click="removeDataset(index)"
+            ></button>
+          </div>
+        </div>
+        <MultiSelect
+          :minimumCharacterBeforeSuggest="2"
+          @change="addDataset"
+          :placeholder="$t('Look for a dataset')"
+          :searchPlaceholder="$t('Search a dataset...')"
+          suggestUrl="/datasets/suggest/"
+        />
+        <p class="fr-hr-or w-100 text-transform-lowercase fr-text--regular">
+          <span class="fr-hr-or-text">{{ $t('or') }}</span>
+        </p>
+        <InputGroup
+          :label="$t('Link to the dataset')"
+          :placeholder="'https://...'"
+          class="w-100"
+          v-model="linkedDataset"
+          @change="getLinkedDataset"
+        />
+        <div v-if="datasetNotFound" class="fr-col bg-contrast-grey text-align-center fr-p-2v">
+          <p class="fr-text--md fr-text--bold">{{ t('No dataset associated to that link has been found') }}</p>
+        </div>
+      </fieldset>
+      <Alert type="error" v-if="errors.length" class="fr-mt-n2w fr-mb-2w">
+        <template #title>{{ t("An error occured | Some errors occured", errors.length) }}</template>
+        <ul v-if="errors.length > 1">
+          <li v-for="error in errors">{{ error }}</li>
+        </ul>
+        <p v-else> {{ errors[0] }}</p>
+      </Alert>
+      <div class="fr-grid-row fr-grid-row--right">
+        <button class="fr-btn" @click="submit">
+          {{ $t("Next") }}
+        </button>
       </div>
-  </Container>
+    </Container>
 </template>
 <script setup lang="ts">
 import { ref, toValue } from 'vue';
