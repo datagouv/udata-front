17e661a066ec57149c92a9b4eb4a660654ac53bf
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2024-06-18 12:08:44 +0200

    fix: qa

--- udata_front/frontend/markdown.py
+++ udata_front/frontend/markdown.py
@@ -1,11 +1,13 @@
 from udata.frontend.markdown import mistune, UDataMarkdown
 
+
 class Renderer(mistune.Renderer):
     def table(self, header, body):
         return (
             '<div class=\"fr-table\">\n<table>\n<thead>\n%s</thead>\n'
             '<tbody>\n%s</tbody>\n</table>\n</div>\n'
         ) % (header, body)
+
 
 class UDataFrontMarkdown(UDataMarkdown):
     """Consistent with Flask's extensions signature."""
@@ -14,6 +16,7 @@ class UDataFrontMarkdown(UDataMarkdown):
         app.jinja_env.filters['markdown'] = self.__call__
         renderer = Renderer(escape=False, hard_wrap=True)
         self.markdown = mistune.Markdown(renderer=renderer)
+
 
 def init_app(app):
     app.extensions['markdown'] = UDataFrontMarkdown(app)
