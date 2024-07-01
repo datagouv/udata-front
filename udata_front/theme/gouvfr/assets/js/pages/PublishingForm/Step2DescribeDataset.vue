df536c299a32f2cc9ae491b51ca4ab14b93692f9
Kevin Cluzel <kevin.cluzel@hotmail.fr>
2024-07-01 09:28:26 +0200

    fix: correct syntax

--- udata_front/theme/gouvfr/assets/js/pages/PublishingForm/Step2DescribeDataset.vue
+++ udata_front/theme/gouvfr/assets/js/pages/PublishingForm/Step2DescribeDataset.vue
@@ -427,7 +427,7 @@ const requiredRules = {
   description: { required },
   frequency: { required },
   title: { required },
-  owned: { custom: checkOwned }
+  owned: { custom: checkOwned },
 };
 
 const warningRules = {
