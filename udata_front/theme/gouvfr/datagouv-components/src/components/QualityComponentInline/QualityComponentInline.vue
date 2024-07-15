009813adda6eaa0eb99fce428bf0ae88d7e60c6e
Kevin Cluzel <kevin.cluzel@hotmail.fr>
2024-07-15 16:16:14 +0200

    fix: :lipstick: move new css class to udata-front

--- udata_front/theme/gouvfr/datagouv-components/src/components/QualityComponentInline/QualityComponentInline.vue
+++ udata_front/theme/gouvfr/datagouv-components/src/components/QualityComponentInline/QualityComponentInline.vue
@@ -2,7 +2,7 @@
     <div class="fr-m-0 fr-grid-row fr-grid-row--middle fr-text--sm text-mention-grey">
         <div class="fr-grid-row fr-grid-row--middle">
           <Toggletip
-              class="fr-btn fr-btn--tertiary-no-outline fr-btn--xs fr-icon-information-line fr-icon--sm fr-mt-1v"
+              class="fr-btn fr-btn--tertiary-no-outline fr-btn--xs text-grey-380 fr-btn--darker-hover fr-icon-information-line fr-icon--sm fr-mt-1v"
           >
               <template #toggletip>
                   <h5 class="fr-text--sm fr-my-0">{{$t("Metadata quality:")}}</h5>
