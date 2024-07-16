81180746d3ef5e9997b1732569081040eeca7bac
Kevin Cluzel <kevin.cluzel@hotmail.fr>
2024-07-16 13:40:19 +0200

    fix: various css fixes

--- udata_front/theme/gouvfr/datagouv-components/src/components/DatasetCard/index.ts
+++ udata_front/theme/gouvfr/datagouv-components/src/components/DatasetCard/index.ts
@@ -3,7 +3,6 @@ import "../button.less";
 import "../dataset.less";
 import "../logo.less";
 import "../links.less";
-import "../typography.less";
 import DatasetCard from "./DatasetCard.vue";
 
 export { DatasetCard };
--- udata_front/theme/gouvfr/datagouv-components/src/composables/useLicense.ts
+++ udata_front/theme/gouvfr/datagouv-components/src/composables/useLicense.ts
@@ -2,8 +2,6 @@ import {ref, unref} from "vue";
 import fetchLicenses from "../api/licenses";
 import { type License } from "../types/licenses";
 
-/** TODO : to be removed when the component moved to data.gouv.fr-components */
-
 export default function useLicense(id: string) {
   id = unref(id);
   const license = ref<License | null>(null);
