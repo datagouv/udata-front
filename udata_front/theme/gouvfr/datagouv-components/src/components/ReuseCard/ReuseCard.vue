cd10c3aec07efcc916ab26e4981c59bb401bf6bc
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2024-08-19 10:51:47 +0200

    fix: visual discrepencies

--- udata_front/theme/gouvfr/datagouv-components/src/components/ReuseCard/ReuseCard.vue
+++ udata_front/theme/gouvfr/datagouv-components/src/components/ReuseCard/ReuseCard.vue
@@ -2,60 +2,65 @@
   <article class="fr-card fr-card--no-icon fr-enlarge-link">
     <div class="fr-card__body">
       <div class="fr-card__content fr-px-2w fr-py-1v">
-        <h3 class="fr-card__title fr-text--bold fr-mt-1v fr-mb-0">
+        <h3 class="fr-card__title fr-text--md fr-mt-1v fr-mb-0">
           <AppLink :to="reuseUrl">
             {{ truncate(reuse.title, 55) }}
           </AppLink>
         </h3>
-        <div class="fr-card__desc fr-mt-1v text-mention-grey">
-          <p class="fr-mb-0">
-            <template v-if="reuse.organization">
-              <span class="not-enlarged dash-after-sm">
-                <AppLink class="fr-link" v-if="organizationUrl" :to="organizationUrl">
-                  <OrganizationNameWithCertificate :organization="reuse.organization" />
-                </AppLink>
-                <OrganizationNameWithCertificate v-else :organization="reuse.organization" />
-              </span>
-            </template>
-            <span class="dash-after-sm" v-else>
-              {{ ownerName }}
+        <div class="fr-card__desc fr-mt-0 text-mention-grey">
+          <p class="fr-text--sm fr-mb-0">
+            <span class="not-enlarged" v-if="reuse.organization">
+              <AppLink class="fr-link" v-if="organizationUrl" :to="organizationUrl">
+                <OrganizationNameWithCertificate :organization="reuse.organization" />
+              </AppLink>
+              <OrganizationNameWithCertificate v-else :organization="reuse.organization" />
+              &mdash;
             </span>
-            {{ t('Published {date}', {date: formatRelativeIfRecentDate(reuse.created_at)}) }}
+            <span v-else>
+              {{ ownerName }} &mdash;
+            </span>
+            {{ t('published {date}', { date: formatRelativeIfRecentDate(reuse.created_at) }) }}
           </p>
-          <div class="fr-grid-row fr-grid-row--middle fr-mt-1v fr-mr-1v">
-            <span class="dash-after-sm">
+          <div class="fr-grid-row fr-grid-row--middle">
+            <p class="fr-text--sm fr-my-0 dash-after-sm">
               {{ reuseType }}
-            </span>
-            <p class="fr-text--sm fr-my-0">
-              <span class="fr-icon-star-line fr-icon--sm fr-px-1v" aria-hidden="true"></span>{{ reuse.metrics.followers }}
             </p>
-            <p class="fr-text--sm fr-my-0">
-              <span class="fr-icon-eye-line fr-icon--sm fr-px-1v" aria-hidden="true"></span>{{ reuse.metrics.views }}
+            <p class="fr-text--sm fr-my-0" :aria-label="t('{n} followers', reuse.metrics.followers)">
+              <span class="fr-icon-star-line fr-icon--xs fr-px-1v" aria-hidden="true"></span>{{ reuse.metrics.followers }}
+            </p>
+            <p class="fr-text--sm fr-my-0" :aria-label="t('{n} views', reuse.metrics.views)">
+              <span class="fr-icon-eye-line fr-icon--xs fr-px-1v" aria-hidden="true"></span>{{ reuse.metrics.views }}
             </p>
           </div>
         </div>
       </div>
     </div>
     <div class="fr-card__header">
-      <div class="absolute top-0 fr-mt-2w fr-ml-2w" v-if="reuse.private || reuse.archived">
-        <p class="fr-badge fr-badge--sm fr-badge--mention-grey text-grey-380" v-if="reuse.private">
-          <span class="fr-icon-lock-line fr-icon--sm">
-            {{ t('Private') }}
-          </span>
-        </p>
-        <p class="fr-badge fr-badge--sm fr-badge--mention-grey text-grey-380" v-else-if="reuse.archived">
-          <span class="fr-icon-lock-line fr-icon--sm">
-            {{ t('Archived') }}
-          </span>
-        </p>
+      <div class="fr-card__img">
+        <Placeholder
+          class="fr-responsive-img reuse-ratio"
+          alt=""
+          type="reuse"
+          :src="reuse.image"
+          :size="320"
+        />
       </div>
-      <Placeholder
-        class="fr-responsive-img reuse-ratio"
-        alt=""
-        type="reuse"
-        :src="reuse.image"
-        :size="80"
-      />
+      <ul class="fr-badges-group" v-if="reuse.private || reuse.archived">
+        <li  v-if="reuse.private">
+          <p class="fr-badge fr-badge--sm fr-badge--mention-grey text-grey-380">
+            <span class="fr-icon-lock-line fr-icon--xs">
+            </span>
+            {{ t('Private') }}
+          </p>
+        </li>
+        <li v-else-if="reuse.archived">
+          <p class="fr-badge fr-badge--sm fr-badge--mention-grey text-grey-380">
+            <span class="fr-icon-lock-line fr-icon--xs">
+            </span>
+            {{ t('Archived') }}
+          </p>
+        </li>
+      </ul>
     </div>
   </article>
 </template>
@@ -99,7 +104,7 @@ const { label: reuseType } = useReuseType(() => props.reuse.type);
 </script>
 
 <style>
-.reuse-ratio {
+.fr-card__img img.reuse-ratio {
   aspect-ratio: 1.4;
   object-fit: cover;
 };
