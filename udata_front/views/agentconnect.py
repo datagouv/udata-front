7a2baf92107b2dec4402b5c51d2dfbb5cf09e46d
Mathieu Agopian <mathieu@agopian.info>
2024-08-23 17:31:56 +0200

    Use the latest authlib==1.3.1 to ease the userinfo decoding process

--- udata_front/views/agentconnect.py
+++ udata_front/views/agentconnect.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import abort, url_for, redirect
+from flask import abort, current_app, redirect, url_for
 from udata.i18n import I18nBlueprint
 from udata_front.frontend import oauth
 from udata.models import datastore
@@ -17,7 +17,9 @@ def login():
         abort(404)
 
     redirect_uri = url_for('agentconnect.auth', _external=True)
-    return client.authorize_redirect(redirect_uri)
+    if current_app.config.get('AGENTCONNECT_REDIRECT_URL'):
+        redirect_uri = current_app.config.get('AGENTCONNECT_REDIRECT_URL')
+    return client.authorize_redirect(redirect_uri, acr_values='eidas1')
 
 
 @blueprint.route('/auth', endpoint='auth')
@@ -27,15 +29,27 @@ def auth():
         abort(404)
 
     token = client.authorize_access_token()
-    agentconnect_user = token.get('userinfo')
-    if not agentconnect_user:
-        agentconnect_user = client.userinfo()
-    user = datastore.find_user(email=agentconnect_user['email'])
+    # /!\ DIRTY HACK.
+    # authlib expects the userinfo to either be in the token["id_token"] as a jwt...
+    # but in this case, it's not there, it's some other information.
+    # We thus need to go get the userinfo from the userinfo_endpoint, but authlib
+    # expects it to be plain json. However, agentconnect returns a jwt.
+    # So we can't use authlib's client.userinfo() helper, we need to do it ourselves.
+    metadata = client.load_server_metadata()
+    resp = client.get(metadata["userinfo_endpoint"])
+    resp.raise_for_status()
+    # Create a new token that `client.parse_id_token` expects. Replace the initial
+    # `id_token` with the jwt we received from the `userinfo_endpoint`.
+    userinfo_token = token.copy()
+    userinfo_token["id_token"] = resp.content
+    agentconnect_user = client.parse_id_token(userinfo_token, nonce=None)
+    # We now have the user information decoded from the jwt, ready to be used.
+    user = datastore.find_user(email=agentconnect_user["email"])
     if not user:
         user = datastore.create_user(
             email=agentconnect_user['email'],
             first_name=agentconnect_user.get('given_name'),
-            last_name=agentconnect_user.get('family_name'),
+            last_name=agentconnect_user.get('usual_name'),
             confirmed_at=datetime.now()
             )
 
