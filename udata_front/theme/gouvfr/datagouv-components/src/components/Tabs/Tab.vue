3ce597da94f299df56270bd4e8cf1511fc32059b
Thibaud Dauce <git@hadibut.fr>
2024-08-28 11:12:16 +0200

    Add Tabs component

---
+++ udata_front/theme/gouvfr/datagouv-components/src/components/Tabs/Tab.vue
@@ -0,0 +1,47 @@
+<template>
+      <Tab as="template" v-slot="{ selected }">
+        <button
+            class="fr-segmented__element"
+            :class="{ 'selected': selected }"
+        >
+            <div class="inner">
+                <slot></slot>
+            </div>
+        </button>
+      </Tab>
+</template>
+<script setup lang="ts">
+import { Tab } from '@headlessui/vue'
+</script>
+<style scoped>
+button {
+    display: inline-flex;
+    align-items: center;
+    border-radius: .25rem;
+    font-size: 1rem;
+    font-weight: 500;
+    line-height: 1.5rem;
+    padding: .25rem 0.25rem;
+    white-space: nowrap;
+}
+
+button.selected {
+    box-shadow: inset 0 0 0 1px var(--border-active-blue-france);
+    color: var(--text-active-blue-france);
+    z-index: 1;
+}
+
+button:hover {
+    background-color: transparent;
+}
+
+button .inner {
+    width: 100%;
+    padding: .25rem .75rem;
+    border-radius: .25rem;
+}
+
+button:not(.selected):hover .inner {
+    background-color: var(--hover);
+}
+</style>
---
+++ udata_front/theme/gouvfr/datagouv-components/src/components/Tabs/TabGroup.vue
@@ -0,0 +1,6 @@
+<template>
+    <TabGroup><slot></slot></TabGroup>
+</template>
+<script setup lang="ts">
+import { TabGroup } from '@headlessui/vue'
+</script>
