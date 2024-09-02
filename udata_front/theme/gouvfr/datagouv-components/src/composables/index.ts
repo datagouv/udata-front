aa2fa7d286dc864f9249db9ad4b1b22ec26c5aef
Thibaud Dauce <git@hadibut.fr>
2024-09-02 13:13:39 +0200

    Remove useResourceImage

--- udata_front/theme/gouvfr/datagouv-components/src/composables/index.ts
+++ udata_front/theme/gouvfr/datagouv-components/src/composables/index.ts
@@ -7,7 +7,6 @@ import useUserAvatar from "./useUserAvatar";
 import useOrganizationCertified, { CERTIFIED } from "./organizations/useOrganizationCertified";
 import useOrganizationType, { hasBadge, isType, ASSOCIATION, COMPANY, LOCAL_AUTHORITY, PUBLIC_SERVICE, type OrganizationTypes } from "./organizations/useOrganizationType";
 import useOwnerName from "./organizations/useOwnerName";
-import useResourceImage from "./resources/useResourceImage";
 import useSchema from "./resources/useSchema";
 export {
   hasBadge,
@@ -19,7 +18,6 @@ export {
   useOrganizationCertified,
   useOrganizationType,
   useOwnerName,
-  useResourceImage,
   useSchema,
   useTabs,
   useToast,
