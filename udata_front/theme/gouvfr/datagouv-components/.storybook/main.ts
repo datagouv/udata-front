6cafcc8db82e57ce34742bd9f610263ace9c7f74
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2024-09-02 11:16:22 +0200

    fix: svg not static

--- udata_front/theme/gouvfr/datagouv-components/.storybook/main.ts
+++ udata_front/theme/gouvfr/datagouv-components/.storybook/main.ts
@@ -11,7 +11,7 @@ const config : StorybookConfig = {
     name: "@storybook/vue3-vite",
     options: {},
   },
-  staticDirs: ['./msw'],
+  staticDirs: ['./msw', '../../templates/svg'],
   docs: {
     autodocs: "tag",
   },
