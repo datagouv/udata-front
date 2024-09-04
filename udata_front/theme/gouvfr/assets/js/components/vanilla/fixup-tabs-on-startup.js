f5a4252423dda14380858a6a1ae6697394c6b1ca
Thibaud Dauce <git@hadibut.fr>
2024-09-04 11:32:14 +0200

    Update tab on back button

--- udata_front/theme/gouvfr/assets/js/components/vanilla/fixup-tabs-on-startup.js
+++ udata_front/theme/gouvfr/assets/js/components/vanilla/fixup-tabs-on-startup.js
@@ -1,13 +1,43 @@
+/** @param button {HTMLElement} */
+function getButtonUrlHash(button)
+{
+  return `#/${button.dataset.updateUrl || button.getAttribute("aria-controls")}`
+}
+
+/** @param button {HTMLElement} */
+function selectTabFromButton(button)
+{
+  const tabPanelId = button.getAttribute('aria-controls');
+  if (! tabPanelId) {
+    console.error("TabButton without `aria-controls` ID.", button)
+  }
+
+  const tabPanel = document.getElementById(tabPanelId);
+  if (! tabPanel) {
+    console.error(`Unknown TabPanel with id '${tabPanelId}'.`, button)
+  }
+
+  dsfr(tabPanel).tabPanel.disclose()
+}
+
 export default function fixupTabsOnStartup()
 {
+  /** @type {NodeListOf<HTMLElement>} */
+  const tabButtons = document.querySelectorAll("[data-update-url]")
+
+  window.addEventListener("hashchange", () => {
+    tabButtons.forEach((button) => {
+      if (window.location.hash.includes(getButtonUrlHash(button))) {
+        selectTabFromButton(button)
+      }
+    })
+  });
+
   // Need to add a random setTimeout of a few thousands of ms to wait 
   // for DSFR to be ready to listen to our `.disclose()` call.
   setTimeout(() => {
-    /** @type {NodeListOf<HTMLElement>} */
-    const tabButtons = document.querySelectorAll("[data-update-url]")
-
     tabButtons.forEach((button) => {
-      const hash = `#/${button.dataset.updateUrl || button.getAttribute("aria-controls")}`;
+      const hash = getButtonUrlHash(button);
       const oldHash = `#${button.dataset.previousUrl}`;
 
       if (oldHash && window.location.hash.startsWith(oldHash)) {
@@ -17,17 +47,7 @@ export default function fixupTabsOnStartup()
       }
 
       if (window.location.hash.includes(hash)) {
-        const tabPanelId = button.getAttribute('aria-controls');
-        if (! tabPanelId) {
-          console.error("TabButton without `aria-controls` ID.", button)
-        }
-
-        const tabPanel = document.getElementById(tabPanelId);
-        if (! tabPanel) {
-          console.error(`Unknown TabPanel with id '${tabPanelId}'.`, button)
-        }
-
-        dsfr(tabPanel).tabPanel.disclose()
+        selectTabFromButton(button)
       }
 
       button.addEventListener('click', () => {
