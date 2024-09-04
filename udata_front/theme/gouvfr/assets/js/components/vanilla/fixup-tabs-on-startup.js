9ecbb8c1dff29ae120d308dc4a067f9bb85c3c77
Thibaud Dauce <git@hadibut.fr>
2024-09-04 11:46:18 +0200

    Listen for old hash too on hash change

--- udata_front/theme/gouvfr/assets/js/components/vanilla/fixup-tabs-on-startup.js
+++ udata_front/theme/gouvfr/assets/js/components/vanilla/fixup-tabs-on-startup.js
@@ -4,20 +4,33 @@ function getButtonUrlHash(button)
   return `#/${button.dataset.updateUrl || button.getAttribute("aria-controls")}`
 }
 
-/** @param button {HTMLElement} */
-function selectTabFromButton(button)
+/** @param tabButtons {NodeListOf<HTMLElement>} */
+function selectCorrectTab(tabButtons)
 {
-  const tabPanelId = button.getAttribute('aria-controls');
-  if (! tabPanelId) {
-    console.error("TabButton without `aria-controls` ID.", button)
-  }
+  tabButtons.forEach((button) => {
+    const hash = getButtonUrlHash(button);
+    const oldHash = `#${button.dataset.previousUrl}`;
 
-  const tabPanel = document.getElementById(tabPanelId);
-  if (! tabPanel) {
-    console.error(`Unknown TabPanel with id '${tabPanelId}'.`, button)
-  }
+    if (oldHash && window.location.hash.startsWith(oldHash)) {
+      // Remove the old hash prefix
+      const subId = window.location.hash.slice(oldHash.length)
+      window.location.hash = `${hash}/${subId}`;
+    }
 
-  dsfr(tabPanel).tabPanel.disclose()
+    if (window.location.hash.includes(hash)) {
+      const tabPanelId = button.getAttribute('aria-controls');
+      if (! tabPanelId) {
+        console.error("TabButton without `aria-controls` ID.", button)
+      }
+    
+      const tabPanel = document.getElementById(tabPanelId);
+      if (! tabPanel) {
+        console.error(`Unknown TabPanel with id '${tabPanelId}'.`, button)
+      }
+    
+      dsfr(tabPanel).tabPanel.disclose()
+    }
+  })
 }
 
 export default function fixupTabsOnStartup()
@@ -26,38 +39,26 @@ export default function fixupTabsOnStartup()
   const tabButtons = document.querySelectorAll("[data-update-url]")
 
   window.addEventListener("hashchange", () => {
-    tabButtons.forEach((button) => {
-      if (window.location.hash.includes(getButtonUrlHash(button))) {
-        selectTabFromButton(button)
+    selectCorrectTab(tabButtons)
+  });
+
+  // Listen for changes to update the hash
+  tabButtons.forEach((button) => {
+    const hash = getButtonUrlHash(button);
+    
+    button.addEventListener('click', () => {
+      if (! window.location.hash.includes(hash)) {
+        let url = new URL(window.location.href);
+        url.hash = hash;
+        window.history.pushState(null, "", url);
+        globalThis._paq?.push(['trackEvent', 'Move page to hash', window.location.pathname, hash]);
       }
     })
-  });
+  })
 
   // Need to add a random setTimeout of a few thousands of ms to wait 
   // for DSFR to be ready to listen to our `.disclose()` call.
   setTimeout(() => {
-    tabButtons.forEach((button) => {
-      const hash = getButtonUrlHash(button);
-      const oldHash = `#${button.dataset.previousUrl}`;
-
-      if (oldHash && window.location.hash.startsWith(oldHash)) {
-        // Remove the old hash prefix
-        const subId = window.location.hash.slice(oldHash.length)
-        window.location.hash = `${hash}/${subId}`;
-      }
-
-      if (window.location.hash.includes(hash)) {
-        selectTabFromButton(button)
-      }
-
-      button.addEventListener('click', () => {
-        if (! window.location.hash.includes(hash)) {
-          let url = new URL(window.location.href);
-          url.hash = hash;
-          window.history.pushState(null, "", url);
-          globalThis._paq?.push(['trackEvent', 'Move page to hash', window.location.pathname, hash]);
-        }
-      })
-    })
+    selectCorrectTab(tabButtons)
   }, 100)
 }
