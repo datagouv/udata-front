607862c2cd0df1fd1ba168d34f0d6f0d3b8e27d0
Nicolas Kempf Le Stanc <nicolas@conciergerie.dev>
2024-09-05 16:21:47 +0200

    ci: missing deps

--- udata_front/theme/gouvfr/assets/js/components/AdminMembershipRequest/AdminMembershipRequest.stories.ts
+++ udata_front/theme/gouvfr/assets/js/components/AdminMembershipRequest/AdminMembershipRequest.stories.ts
@@ -1,5 +1,5 @@
 import { withActions } from '@storybook/addon-actions/decorator';
-import { userEvent, expect, screen, within, waitFor, fn } from '@storybook/test';
+import { userEvent, expect, screen, within, fn } from '@storybook/test';
 import type { Meta, StoryObj } from "@storybook/vue3";
 import AdminMembershipRequest from './AdminMembershipRequest.vue';
 import { DefaultIdenticonAvatar } from "../discussions/Avatar/Avatar.stories";
