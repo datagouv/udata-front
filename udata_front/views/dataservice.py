92005469239f9994f9e3838a7a3c956439303b45
Thibaud Dauce <ThibaudDauce@users.noreply.github.com>
2024-10-21 12:05:21 +0200

    Paginate datasets in dataservice display (#560)
    While waiting for the refactor of this page in JS, paginate in Jinja.
    
    See https://github.com/datagouv/data.gouv.fr/issues/1467
    See https://github.com/datagouv/udata-front/pull/548
    See https://github.com/opendatateam/udata/pull/3156

--- udata_front/views/dataservice.py
+++ udata_front/views/dataservice.py
@@ -7,6 +7,7 @@ from udata.core.dataservices.permissions import DataserviceEditPermission
 from udata.core.site.models import current_site
 from udata.i18n import I18nBlueprint, gettext as _
 from udata.sitemap import sitemap
+from flask_mongoengine.pagination import Pagination
 
 from udata_front import theme
 from udata_front.theme import render as render_template
@@ -81,6 +82,20 @@ class DataserviceDetailView(DataserviceView, DetailView):
                 abort(404)
             elif self.dataservice.deleted_at:
                 abort(410)
+
+        datasets = Pagination(
+            self.dataservice.datasets,
+            request.args.get('datasets_page', 1, type=int),
+            8,
+        )
+
+        context['datasets'] = datasets
+
+        # Load the lazy references (could be better to fetch them in one request instead of 8)
+        # We need to have a seperate variable because .fetch() return the object instead of
+        # setting it inside the LazyReference object.
+        context['datasets_items'] = [dataset.fetch() for dataset in datasets.items]
+
         context['can_edit'] = DataserviceEditPermission(self.dataservice)
         return context
 
