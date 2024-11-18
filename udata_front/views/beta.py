6f2ffde4b34e1608c34c91dfca58fb73bad3f615
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2024-11-18 16:32:31 +0100

    fix: linting

--- udata_front/views/beta.py
+++ udata_front/views/beta.py
@@ -3,14 +3,19 @@ from udata_front.views.base import LoginOnlyView
 
 blueprint = I18nBlueprint('beta', __name__, url_prefix='/beta')
 
+
 # This is a temporary file and route to keep the existing admin in place
 # and allowing users with the link to view the new admin
 @blueprint.route('/admin/', endpoint='beta-admin')
 class AdminView(LoginOnlyView):
     template_name = 'beta/admin.html'
 
-@blueprint.route('/admin/organizations/<organization_id>/dataservices/<dataservice_id>', endpoint='beta-admin-org-dataservice')
-class AdminViewDataservice(LoginOnlyView):
+
+@blueprint.route(
+    '/admin/organizations/<organization_id>/dataservices/<dataservice_id>',
+    endpoint='beta-admin-org-dataservice'
+)
+class AdminViewOrgDataservice(LoginOnlyView):
     template_name = 'beta/admin.html'
 
     def get_context(self, **kwargs):
@@ -20,8 +25,9 @@ class AdminViewDataservice(LoginOnlyView):
         context['dataservice_id'] = kwargs.get('dataservice_id')
         return context
 
+
 @blueprint.route('/admin/me/dataservices/<dataservice_id>', endpoint='beta-admin-me-dataservice')
-class AdminViewDataservice(LoginOnlyView):
+class AdminViewMeDataservice(LoginOnlyView):
     template_name = 'beta/admin.html'
 
     def get_context(self, **kwargs):
