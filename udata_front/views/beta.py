b3639f36f14d86f64b8dfba1a95a444523edf2fa
geoffreyaldebert <geoffrey.aldebert@gmail.com>
2024-11-18 16:09:46 +0100

    feat: add edit button in dataservice public page that redirect user to new admin

--- udata_front/views/beta.py
+++ udata_front/views/beta.py
@@ -8,3 +8,25 @@ blueprint = I18nBlueprint('beta', __name__, url_prefix='/beta')
 @blueprint.route('/admin/', endpoint='beta-admin')
 class AdminView(LoginOnlyView):
     template_name = 'beta/admin.html'
+
+@blueprint.route('/admin/organizations/<organization_id>/dataservices/<dataservice_id>', endpoint='beta-admin-org-dataservice')
+class AdminViewDataservice(LoginOnlyView):
+    template_name = 'beta/admin.html'
+
+    def get_context(self, **kwargs):
+        context = super().get_context(**kwargs)
+        # Extract parameters from kwargs
+        context['organization_id'] = kwargs.get('organization_id')
+        context['dataservice_id'] = kwargs.get('dataservice_id')
+        return context
+
+@blueprint.route('/admin/me/dataservices/<dataservice_id>', endpoint='beta-admin-me-dataservice')
+class AdminViewDataservice(LoginOnlyView):
+    template_name = 'beta/admin.html'
+
+    def get_context(self, **kwargs):
+        context = super().get_context(**kwargs)
+        # Extract parameters from kwargs
+        context['organization_id'] = kwargs.get('organization_id')
+        context['dataservice_id'] = kwargs.get('dataservice_id')
+        return context
