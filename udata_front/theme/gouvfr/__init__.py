d6960d1d0d7858496dd631bbb99b71795c19b499
maudetes <maudet.estelle@gmail.com>
2025-04-11 11:32:29 +0200

    Use SUPPORT_URL config everywhere
    We'll easily modify its value following https://github.com/datagouv/cdata/pull/365

--- udata_front/theme/gouvfr/__init__.py
+++ udata_front/theme/gouvfr/__init__.py
@@ -107,7 +107,7 @@ nav.Bar('gouvfr_opendata', opendata_links)
 platform_links = [
     nav.Item(_('Guides'), None, url=current_app.config.get('GUIDES_URL', '#')),
     nav.Item(_('Roadmap and news'), 'gouvfr.show_page', args={'slug': 'roadmap'}),
-    nav.Item(_('Contact us'), None, url='https://support.data.gouv.fr'),
+    nav.Item(_('Contact us'), None, url=current_app.config['SUPPORT_URL']),
     nav.Item(_('Give us your feedback'), None,
              url=current_app.config.get('FEEDBACK_FORM_URL', '#')),
     nav.Item(_('Statistics'), 'site.dashboard'),
