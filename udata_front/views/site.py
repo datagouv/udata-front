0be1cf5302ec3fede34902d84d9691ccc7c88aaa
Thibaud Dauce <git@hadibut.fr>
2025-04-30 15:16:58 +0200

    Migrate CSV exports to udata

--- udata_front/views/site.py
+++ udata_front/views/site.py
@@ -1,32 +1,17 @@
 import logging
 import requests
 
-from flask import request, redirect, url_for, current_app, abort, make_response
+from flask import request, url_for, current_app, make_response
 from mongoengine.errors import DoesNotExist
 from feedgenerator.django.utils.feedgenerator import Atom1Feed
 
 from udata.app import cache
 from udata.core.activity.models import Activity
-from udata.core.dataservices.csv import DataserviceCsvAdapter
-from udata.core.dataservices.models import Dataservice
-from udata.core.dataset.api import DatasetApiParser
-from udata.core.dataset.csv import ResourcesCsvAdapter
 from udata.core.dataset.models import Dataset
-from udata.core.dataset.search import DatasetSearch
-from udata.core.dataset.tasks import get_queryset as get_csv_queryset
-from udata.core.organization.api import OrgApiParser
-from udata.core.organization.csv import OrganizationCsvAdapter
-from udata.core.organization.models import Organization
 from udata.core.post.models import Post
-from udata.core.reuse.api import ReuseApiParser
-from udata.core.reuse.csv import ReuseCsvAdapter
 from udata.core.reuse.models import Reuse
-from udata.harvest.csv import HarvestSourceCsvAdapter
-from udata.harvest.models import HarvestSource
-from udata.frontend import csv
 from udata.i18n import I18nBlueprint, lazy_gettext as _
 from udata.sitemap import sitemap
-from udata.utils import multi_to_dict
 from udata_front import theme
 
 from udata.core.site.models import current_site
@@ -109,92 +94,6 @@ def home():
     processor = theme.current.get_processor('home')
     context = processor(context)
     return theme.render('home.html', **context)
-
-
-def get_export_url(model):
-    did = current_app.config['EXPORT_CSV_DATASET_ID']
-    dataset = Dataset.objects.get_or_404(id=did)
-    resource = None
-    for r in dataset.resources:
-        if r.extras.get('csv-export:model', '') == model:
-            resource = r
-            break
-    if not resource:
-        abort(404)
-    return resource.url
-
-
-@blueprint.route('/datasets.csv')
-def datasets_csv():
-    # redirect to EXPORT_CSV dataset if feature is enabled and no filter is set
-    exported_models = current_app.config.get('EXPORT_CSV_MODELS', [])
-    if not request.args and 'dataset' in exported_models:
-        return redirect(get_export_url('dataset'))
-    search_parser = DatasetSearch.as_request_parser(store_missing=False)
-    params = search_parser.parse_args()
-    params['facets'] = False
-    datasets = DatasetApiParser.parse_filters(get_csv_queryset(Dataset), params)
-    adapter = csv.get_adapter(Dataset)
-    return csv.stream(adapter(datasets), 'datasets')
-
-
-@blueprint.route('/resources.csv')
-def resources_csv():
-    # redirect to EXPORT_CSV dataset if feature is enabled and no filter is set
-    exported_models = current_app.config.get('EXPORT_CSV_MODELS', [])
-    if not request.args and 'resource' in exported_models:
-        return redirect(get_export_url('resource'))
-    search_parser = DatasetSearch.as_request_parser(store_missing=False)
-    params = search_parser.parse_args()
-    params['facets'] = False
-    datasets = DatasetApiParser.parse_filters(get_csv_queryset(Dataset), params)
-    return csv.stream(ResourcesCsvAdapter(datasets), 'resources')
-
-
-@blueprint.route('/organizations.csv')
-def organizations_csv():
-    params = multi_to_dict(request.args)
-    # redirect to EXPORT_CSV dataset if feature is enabled and no filter is set
-    exported_models = current_app.config.get('EXPORT_CSV_MODELS', [])
-    if not params and 'organization' in exported_models:
-        return redirect(get_export_url('organization'))
-    params['facets'] = False
-    organizations = OrgApiParser.parse_filters(get_csv_queryset(Organization), params)
-    return csv.stream(OrganizationCsvAdapter(organizations), 'organizations')
-
-
-@blueprint.route('/reuses.csv')
-def reuses_csv():
-    params = multi_to_dict(request.args)
-    # redirect to EXPORT_CSV dataset if feature is enabled and no filter is set
-    exported_models = current_app.config.get('EXPORT_CSV_MODELS', [])
-    if not params and 'reuse' in exported_models:
-        return redirect(get_export_url('reuse'))
-    params['facets'] = False
-    reuses = ReuseApiParser.parse_filters(get_csv_queryset(Reuse), params)
-    return csv.stream(ReuseCsvAdapter(reuses), 'reuses')
-
-
-@blueprint.route('/dataservices.csv')
-def dataservices_csv():
-    params = multi_to_dict(request.args)
-    # redirect to EXPORT_CSV dataset if feature is enabled and no filter is set
-    exported_models = current_app.config.get('EXPORT_CSV_MODELS', [])
-    if not params and 'dataservice' in exported_models:
-        return redirect(get_export_url('dataservice'))
-    params['facets'] = False
-    dataservices = Dataservice.apply_sort_filters(get_csv_queryset(Dataservice))
-    return csv.stream(DataserviceCsvAdapter(dataservices), 'dataservices')
-
-
-@blueprint.route('/harvests.csv')
-def harvests_csv():
-    # redirect to EXPORT_CSV dataset if feature is enabled
-    exported_models = current_app.config.get('EXPORT_CSV_MODELS', [])
-    if 'harvest' in exported_models:
-        return redirect(get_export_url('harvest'))
-    adapter = HarvestSourceCsvAdapter(get_csv_queryset(HarvestSource).order_by('created_at'))
-    return csv.stream(adapter, 'harvest')
 
 
 class SiteView(object):
